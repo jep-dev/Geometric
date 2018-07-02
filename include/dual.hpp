7709974bd42dba3e7bbe71babbd7bbe028f2dc5d
John Petersen <jepetersen.dev@gmail.com>
2018-07-02 02:29:16 -0400

    Dual quaternion (flat representation)

---
+++ include/dual.hpp
@@ -0,0 +1,60 @@
+#ifndef DUAL_HPP
+#define DUAL_HPP
+
+#include <algorithm>
+
+template<class S> struct DualQuaternion {
+	using type = S;
+	S s, t, u, v, w, x, y, z;
+	const S& operator[](unsigned i) const {
+		switch(i) {
+			case 7: case 'K': return z;
+			case 6: case 'J': return y;
+			case 5: case 'I': return x;
+			case 4: case 'E': return w;
+			case 3: case 'k': return v;
+			case 2: case 'j': return u;
+			case 1: case 'i': return t;
+			default: return s;
+		}
+	}
+	S& operator[](unsigned i) {
+		switch(i) {
+			case 7: case 'K': return z;
+			case 6: case 'J': return y;
+			case 5: case 'I': return x;
+			case 4: case 'E': return w;
+			case 3: case 'k': return v;
+			case 2: case 'j': return u;
+			case 1: case 'i': return t;
+			default: return s;
+		}
+	}
+	type square(void) const { return *this * *this; }
+	type lengthSquared(void) const { return s*s + t*t + u*u + v*v + w*w + x*x + y*y + z*z; }
+	type length(void) const { return sqrt(lengthSquared()); }
+	DualQuaternion operator*(void) const { return {s, -t, -u, -v, -w, x, y, z}; }
+	DualQuaternion operator-(void) const { return {-s, -t, -u, -v, -w, -x, -y, -z}; }
+	DualQuaternion operator*(DualQuaternion const& d) const {
+		return {
+			s*d.s - t*d.t - u*d.u - v*d.v,  s*d.t + t*d.s + u*d.v - v*d.u,
+			s*d.u + u*d.s - t*d.v + v*d.t,  s*d.v + v*d.s + t*d.u - u*d.t,
+			s*d.w - t*d.x - u*d.y - v*d.z + w*d.s - x*d.t - y*d.u - z*d.v,
+			s*d.x + t*d.w + u*d.z - v*d.y + w*d.t + x*d.s + y*d.v - z*d.u,
+			s*d.y - t*d.z + u*d.w + v*d.x + w*d.u - x*d.v + y*d.s + z*d.t,
+			s*d.z + t*d.y - u*d.x + v*d.w + w*d.v + x*d.u - y*d.t + z*d.s
+		};
+	}
+	template<class C> DualQuaternion operator*(C const& c) const
+		{ return {s*c, t*c, u*c, v*c, w*c, x*c, y*c, z*c}; }
+	template<class C> friend DualQuaternion operator*(C const& c, DualQuaternion const& d)
+		{ return {c*d.s, c*d.t, c*d.u, c*d.v, c*d.w, c*d.x, c*d.y, c*d.z}; }
+	bool operator>(DualQuaternion const& d) const
+		{ return s > d.s && t > d.t && u > d.u && v > d.v
+				&& w > d.w && x > d.x && y > d.y && z > d.z; }
+	bool operator==(DualQuaternion const& d) const
+		{ return s == d.s && t == d.t && u == d.u && v == d.v
+				&& w == d.w && x == d.x && y == d.y && z == d.z; }
+};
+
+#endif
---
+++ include/dual.tpp
@@ -0,0 +1,41 @@
+#ifndef DUAL_TPP
+#define DUAL_TPP
+
+#include "dual.hpp"
+#include "quaternion.hpp"
+#include "quaternion.tpp"
+#include <iostream>
+#include <sstream>
+
+template<class L, class S>
+L& operator<<(L &lhs, DualQuaternion<S> const& rhs) {
+	/*std::ostringstream oss;
+	Quaternion<S,S> p = {rhs.s, rhs.t, rhs.u, rhs.v},
+		q = {rhs.w, rhs.x, rhs.y, rhs.z};
+	oss << p << "+(" << q << ")E";
+	return lhs << oss.str(), lhs;*/
+	/*for(auto i : {'e', 'i', 'j', 'k', 'E', 'I', 'J', 'K'}) {
+		oss << rhs[i] << i << (i == 'e' ? "" : "+");
+	}
+	lhs << oss.str();
+	return lhs;*/
+
+	bool nz = false;
+	std::ostringstream oss;
+	for(auto i : {'e', 'i', 'j', 'k', 'E', 'I', 'J', 'K'}) {
+		auto const& q = rhs[i];
+		auto q0 = q*0;
+		if(q == q0) continue;
+		auto q2 = q*q;
+		if(q2 == q) oss << (nz ? "+" : "");
+		else if(q2 == -q) oss << (nz ? "-" : "-");
+		else if(q > q0) oss << (nz ? "+" : "") << q;
+		else oss << (nz ? "-" : "-") << -q;
+		oss << i;
+		nz = true;
+	}
+	if(!nz) oss << '0';
+	return lhs << oss.str(), lhs;
+}
+
+#endif
