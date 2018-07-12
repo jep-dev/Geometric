b03353a2071f1df09f944f560a0bb9fc01b2b7dd
John Petersen <jepetersen.dev@gmail.com>
2018-07-12 10:56:59 -0400

    Added 'Handled' type and changed return types to Handled instances

--- include/events.hpp
+++ include/events.hpp
@@ -10,30 +10,36 @@ namespace Events {
 	namespace Detail {
 		template<class S> S&& (*Fwd)(S &&) = &std::forward<S>;
 	}
+	struct Handled {
+		typedef enum {
+			CODE_PASS = 1,
+			CODE_QUIT = CODE_PASS << 1,
+			CODE_ERROR = CODE_QUIT << 1,
+				CODE_WARN = CODE_ERROR | CODE_PASS,
+				CODE_FAIL = CODE_ERROR | CODE_QUIT
+		} E_Code;
 
-/* TODO: use Void_t or equivalent to apply SFINAE once per handled event type;
- * with CRTP, the derived type can satisfy the contract of forwarding:
- *
- *   template<class DERIVED, class = void>
- *     Handler -> false_type
- *
- *     template<class DERIVED, class ARG, bool (DERIVED::* F) (ARG)>
- *       Handler<DERIVED, F> -> integral_constant<decltype(F), F>
- *
- *   template<class...>
- *     MultiHandler
- *
- *     template<class... S, class... T>
- *       MultiHandler<Handler<S...>, T...> -> Handler<S...>, MultiHandler<T...>
- *
- */
+		uint32_t code = CODE_PASS;
+		uint64_t timestamp = SDL_GetPerformanceCounter();
+
+		static bool passed(uint32_t h) { return h & CODE_PASS; }
+		static bool quit(uint32_t h) { return h & CODE_QUIT; }
+		static bool errored(uint32_t h) { return h & CODE_ERROR; }
+		static bool warned(uint32_t h) { return (h & CODE_WARN) == CODE_WARN; }
+		static bool failed(uint32_t h) { return (h & CODE_FAIL) == CODE_FAIL; }
+		static bool bad(uint32_t h) { return quit(h) || errored(h); }
+		static bool good(uint32_t h) { return passed(h) && !bad(h); }
+
+		operator bool(void) const { return good(code); }
+	};
+
 	template<class C>
 	struct Handler {
 		template<class... T>
-		bool operator()(SDL_QuitEvent const&, T &&...) { return false; }
+		Handled operator()(SDL_QuitEvent const&, T &&...) { return {Handled::CODE_QUIT}; }
 
 		template<class... T>
-		bool operator()(SDL_Event const& ev, T &&... t) {
+		Handled operator()(SDL_Event const& ev, T &&... t) {
 			using namespace Detail;
 			auto &self = static_cast<C&>(*this);
 			switch(ev.type) {
@@ -62,7 +68,7 @@ namespace Events {
 				case SDL_TEXTEDITING: return self(ev.edit, Fwd<T>(t)...);
 				case SDL_TEXTINPUT: return self(ev.text, Fwd<T>(t)...);
 				case SDL_WINDOWEVENT: return self(ev.window, Fwd<T>(t)...);
-				default: return true;
+				default: return {Handled::CODE_PASS};
 			}
 		}
 		template<class... T>
