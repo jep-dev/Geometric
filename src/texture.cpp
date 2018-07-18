be52e8319b7a91cfb7508d05a2b6a1aa51cf426b
John Petersen <jepetersen.dev@gmail.com>
2018-07-17 21:43:12 -0400

    Added image loading procedure; kind of important

--- src/texture.cpp
+++ src/texture.cpp
@@ -24,6 +24,10 @@ namespace View {
 				return created = sourced = false;
 			}
 		}
+		int width, height;
+		unsigned char *image = SOIL_load_image(fname, &width, &height, 0, SOIL_LOAD_AUTO);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+		SOIL_free_image_data(image);
 		return created = sourced = true;
 	}
 	Texture::Texture(void) {
