e3eba7af1cf5f9cd5f27d1e29b35f86a02289640
John Petersen <jepetersen.dev@gmail.com>
2018-07-19 21:36:24 -0400

    Removed redundant attribute settings

--- src/frame.cpp
+++ src/frame.cpp
@@ -14,10 +14,6 @@ Frame::Frame(void) {
 	ostringstream oss;
 
 	int init = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
-	/*SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, true);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);*/
 
 	static constexpr auto
 		key_major = SDL_GL_CONTEXT_MAJOR_VERSION,
