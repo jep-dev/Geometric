8e004aa0d56631647868b7d2c2e05ab86ec63f2d
John Petersen <jepetersen.dev@gmail.com>
2018-07-19 23:41:44 -0400

    Restored previously ignored alpha size requirement attribute

--- include/context.hpp
+++ include/context.hpp
@@ -9,7 +9,7 @@ namespace View {
 		Minor minor = {3, &meet_attr};
 		Mask mask = {SDL_GL_CONTEXT_PROFILE_CORE, &mask_attr};
 		DoubleBuffer buffered = {true, &meet_attr};
-		AlphaSize alpha = {8, &ignore_attr};
+		AlphaSize alpha = {8, &meet_attr};
 		RedSize red = {8, &meet_attr};
 		GreenSize green = {8, &meet_attr};
 		BlueSize blue = {8, &meet_attr};
