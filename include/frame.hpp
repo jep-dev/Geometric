2049f7c3d963ca7578d33fbd36843053d758a237
John Petersen <jepetersen.dev@gmail.com>
2018-07-21 17:55:54 -0400

    Formatting

--- include/frame.hpp
+++ include/frame.hpp
@@ -7,35 +7,36 @@
 
 namespace View {
 
-	/** Represents a window and associated GL context. */
-	struct Frame {
-	public:
-		int flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL;
-		std::string message;
-	protected:
-		static constexpr int center = SDL_WINDOWPOS_CENTERED;
-		SDL_Window *win = 0;
-		Context ctx;
-	public:
-		/** Stream insertion operator; inserts the most recent message(s)/error(s). */
-		template<class S>
-		friend S& operator<<(S &s, Frame const& f) {
-			if(f.message.length()) s << f.message;
-			return s;
-		}
-		/** Const access of the context. */
-		SDL_GLContext getContext(void) const { return ctx; }
-		/** Const access of the window. */
-		SDL_Window *const getWindow(void) const { return win; }
-		/** Constructor; currently initializes and sets a handful of hard-coded GL attributes */
-		Frame(void);
-		/** Destructor; destroys the stored window. */
-		virtual ~Frame(void) {
-			if(win)
-				SDL_DestroyWindow(win);
-			win = 0;
-		}
-	};
+/** Represents a window and associated GL context. */
+struct Frame {
+public:
+	int flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL;
+	std::string message;
+protected:
+	static constexpr int center = SDL_WINDOWPOS_CENTERED;
+	SDL_Window *win = 0;
+	Context ctx;
+public:
+	/** Stream insertion operator; inserts the most recent message(s)/error(s). */
+	template<class S>
+	friend S& operator<<(S &s, Frame const& f) {
+		if(f.message.length()) s << f.message;
+		return s;
+	}
+	/** Const access of the context. */
+	SDL_GLContext getContext(void) const { return ctx; }
+	/** Const access of the window. */
+	SDL_Window *const getWindow(void) const { return win; }
+	/** Constructor; currently initializes and sets a handful of hard-coded GL attributes */
+	Frame(void);
+	/** Destructor; destroys the stored window. */
+	virtual ~Frame(void) {
+		if(win)
+			SDL_DestroyWindow(win);
+		win = 0;
+	}
+};
+
 }
 
 #endif
