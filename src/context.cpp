21c729d061cf5aeb8efb28a0592f35cf08a465cf
John Petersen <jepetersen.dev@gmail.com>
2018-07-21 18:01:01 -0400

    Formatting

--- src/context.cpp
+++ src/context.cpp
@@ -1,11 +1,13 @@
 #include "context.hpp"
 
 namespace View {
-	Context& Context::set(void) {
-		set_attrs(major, minor, mask, buffered, alpha, red, green, blue);
-		return *this;
-	}
-	Context::operator SDL_GLContext(void) const {
-		return ctx;
-	}
+
+Context& Context::set(void) {
+	set_attrs(major, minor, mask, buffered, alpha, red, green, blue);
+	return *this;
+}
+Context::operator SDL_GLContext(void) const {
+	return ctx;
+}
+
 }
