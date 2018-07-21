c00b7391fd52cb183cf15709ccac6430b9157335
John Petersen <jepetersen.dev@gmail.com>
2018-07-21 18:02:20 -0400

    Removed unnecessary context-forming braces

--- app/dual.cpp
+++ app/dual.cpp
@@ -10,14 +10,12 @@
 
 
 int main(int argc, const char *argv[]) {
-	{
-		DualQuaternion<float> LHS[] = {{1}, {0,1}, {0,0,1}, {0,0,0,1},
-				{0,0,0,0,1}, {0,0,0,0,0,1}, {0,0,0,0,0,0,1}, {0,0,0,0,0,0,0,1}};
-		for(auto const& lhs : LHS) {
-			for(auto const& rhs : LHS) {
-				std::cout << std::setw(3) << (lhs * rhs) << " ";
-			}
-			endl(std::cout);
+	DualQuaternion<float> LHS[] = {{1}, {0,1}, {0,0,1}, {0,0,0,1},
+			{0,0,0,0,1}, {0,0,0,0,0,1}, {0,0,0,0,0,0,1}, {0,0,0,0,0,0,0,1}};
+	for(auto const& lhs : LHS) {
+		for(auto const& rhs : LHS) {
+			std::cout << std::setw(3) << (lhs * rhs) << " ";
 		}
+		endl(std::cout);
 	}
 }
