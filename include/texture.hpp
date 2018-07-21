231eabbafd19f5b48964fd647d7205dd76341ec1
John Petersen <jepetersen.dev@gmail.com>
2018-07-21 17:59:40 -0400

    Formatting

--- include/texture.hpp
+++ include/texture.hpp
@@ -6,28 +6,30 @@
 #include <SOIL/SOIL.h>
 
 namespace View {
-	struct Texture {
-		/*
-		const char *fname = "";
-		GLuint value = 0;
-		int width = 0, height = 0;
-		bool created = false, sourced = false;
-		std::string message;
-		bool source(bool force = true);
-		bool source(const char*, bool force = true);
-		Texture(void);
-		Texture(const char *);
-		*/
-		const char *fname;
-		unsigned int flags;
-		GLuint value;
-		bool created = false, sourced = false;
-		unsigned int line = 0;
-		ILint width = 0, height = 0;
-		std::string message;
-		Texture(const char*, unsigned int = SOIL_FLAG_INVERT_Y | SOIL_FLAG_MIPMAPS);
-		virtual ~Texture(void);
-	};
+
+struct Texture {
+	/*
+	const char *fname = "";
+	GLuint value = 0;
+	int width = 0, height = 0;
+	bool created = false, sourced = false;
+	std::string message;
+	bool source(bool force = true);
+	bool source(const char*, bool force = true);
+	Texture(void);
+	Texture(const char *);
+	*/
+	const char *fname;
+	unsigned int flags;
+	GLuint value;
+	bool created = false, sourced = false;
+	unsigned int line = 0;
+	ILint width = 0, height = 0;
+	std::string message;
+	Texture(const char*, unsigned int = SOIL_FLAG_INVERT_Y | SOIL_FLAG_MIPMAPS);
+	virtual ~Texture(void);
+};
+
 }
 
 #endif
