b5293f54f9551331e30dac57521a675a55f5ad36
John Petersen <jepetersen.dev@gmail.com>
2018-07-24 17:25:53 -0400

    Presenter stub; forwards arguments to Frame member and contains a Model member

---
+++ include/presenter.hpp
@@ -0,0 +1,18 @@
+#ifndef PRESENTER_HPP
+#define PRESENTER_HPP
+
+#include "frame.hpp"
+#include "model.hpp"
+
+template<class S>
+struct Presenter;
+template<class S>
+struct Presenter: Events::Handler<S> {
+	using Events::Handler<S>::operator();
+	View::Frame frame;
+	Model model;
+	template<class... T>
+	Presenter(T &&... t): frame(std::forward<T>(t)...) {}
+};
+
+#endif
