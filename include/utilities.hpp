f40f757575b1c9ff697cb2c22e78fcc6416245ee
John Petersen <jepetersen.dev@gmail.com>
2018-07-26 22:03:11 -0400

    Rewrite of operator^ for Tag types; order and depth of results all appear to be correct now (except for <A>^<B>=<A,B>, which could be <<A,B>> for consistency)

--- include/utilities.hpp
+++ include/utilities.hpp
@@ -45,23 +45,46 @@ Tag<LHS..., RHS...> operator+(Tag<LHS...>, Tag<RHS...>) { return {}; }
 template<class... LN, class... RN>
 Tag<Tag<LN,RN>...> operator*(Tag<LN...>, Tag<RN...>) { return {}; }
 
-/** Outer product; |LHS|*|RHS|=0 -> |LHS*RHS|=0 */
+/*
+// Outer product; |LHS|*|RHS|=0 -> |LHS*RHS|=0 //
 template<class... LN, class... RN>
 Tag<> operator^(Tag<LN...>, Tag<RN...>) { return {}; }
 
-/** Outer product; |LHS|=1 and |RHS|>1 */
+template<class L, class R>
+Tag<L, R> operator^(Tag<L>, Tag<R>) { return {}; }
+
+// Outer product; |LHS|=1 and |RHS|>1 //
 template<class L, class... RN>
 Tag<Tag<L, RN>...> operator^(Tag<L> lhs, Tag<RN...> rhs) { return {}; }
 
-/** Outer product; |LHS|>1 and |RHS|=1 */
+// Outer product; |LHS|>1 and |RHS|=1 //
 template<class... LN, class R>
 Tag<Tag<LN, R>...> operator^(Tag<LN...> lhs, Tag<R> rhs) { return {}; }
 
-/** Outer product; |LHS|>1 and |RHS|>1 */
+// Outer product; |LHS|>1 and |RHS|>1 //
 template<class L0, class L1, class... LN, class R0, class R1, class... RN>
 auto operator^(Tag<L0, L1, LN...> lhs, Tag<R0, R1, RN...> rhs)
 -> Tag<decltype(Tag<L0>{} ^ rhs), decltype(Tag<L1>{} ^ rhs),
 	decltype(Tag<LN>{} ^ rhs)...> { return {}; }
+*/
+
+template<class U>
+auto ext(U &&, Tag<>) -> Tag<> { return {}; }
+template<class X>
+auto ext(Tag<>, X &&) -> Tag<> { return {}; }
+template<class U, class X>
+auto ext(Tag<U>, Tag<X>) -> Tag<U,X> { return {}; }
+template<class U, class... V, class X>
+auto ext(Tag<U, V...>, Tag<X>) -> Tag<Tag<U, X>, Tag<V, X>...> { return {}; }
+template<class U, class X, class... Y>
+auto ext(Tag<U>, Tag<X, Y...>) -> Tag<Tag<U, X>, Tag<U, Y>...> { return {}; }
+template<class U, class... V, class X, class... Y>
+auto ext(Tag<U, V...>, Tag<X, Y...>) -> decltype(Tag<Tag<U, X>, Tag<U, Y>...>{}
+	+ ext(Tag<V...>{}, Tag<X,Y...>{})) { return {}; }
+
+template<class... U, class... X>
+auto operator^(Tag<U...> u, Tag<X...> x) -> decltype(ext(u, x)) { return ext(u, x); }
+
 }
 
 
