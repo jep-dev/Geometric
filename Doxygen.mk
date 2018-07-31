b25f440e67373349984e1bcda1f4242cae7a4857
John Petersen <jepetersen.dev@gmail.com>
2018-07-31 08:39:22 -0400

    Successfully generated tags, but disabled them when final generated docs appeared mostly empty; tags are supposed to be for 'components' which I had interpreted as closer to 'translation units' than 'modules'. Revisit someday.

--- Doxygen.mk
+++ Doxygen.mk
@@ -4,38 +4,58 @@ doxymake=$(doxygen) -g
 
 # Input parameters
 DIR_DOC?=doc/
+DIR_TAG?=$(DIR_DOC)tags/
 FILE_DOC?=$(DIR_DOC)Doxyfile
 
-DOXY_HTML_EXTRA_STYLESHEET=doc/style.css
-DOXY_BUILTIN_STL_SUPPORT?=YES
-DOXY_COLS_IN_ALPHA_INDEX?=5
-DOXY_DOT_TRANSPARENT?=YES
-DOXY_EXCLUDE_PATTERNS?=*.o, *.d, *.h, */obj/*
-DOXY_EXTRACT_ALL?=YES
-DOXY_EXTRACT_PRIVATE?=YES
-DOXY_EXTRACT_PACKAGE?=YES
-#DOXY_GENERATE_MAN?=YES
-DOXY_OUTPUT_DIRECTORY=$(DIR_DOC)
-DOXY_QT_AUTOBRIEF?=YES
-DOXY_MULTILINE_CPP_IS_BRIEF=YES
-DOXY_RECURSIVE?=YES
-DOXY_SHOW_INCLUDE_FILES?=YES
-DOXY_STRIP_FROM_PATH?=./
+HTML_EXTRA_STYLESHEET?=doc/style.css
+BUILTIN_STL_SUPPORT?=YES
+COLS_IN_ALPHA_INDEX?=5
+DOT_TRANSPARENT?=YES
+EXCLUDE_PATTERNS?=*.o, *.d, *.h, */obj/*
+EXTRACT_ALL?=YES
+EXTRACT_PRIVATE?=YES
+EXTRACT_PACKAGE?=YES
+INPUT=$(DIR_HDR) $(DIR_SRC) $(DIR_APP)
+INPUTS=$(foreach I,$(INPUT),$(wildcard $I*))
+NAMES_DOC=$(patsubst %,doxy-tag-%,$(sort $(notdir $(basename $(INPUTS)))))
+OUTPUT_DIRECTORY=$(DIR_DOC)
+QT_AUTOBRIEF?=YES
+MULTILINE_CPP_IS_BRIEF=YES
+RECURSIVE?=YES
+SHOW_INCLUDE_FILES?=YES
+STRIP_FROM_PATH?=./
+WARNINGS=NO
 
-DOXY_WARNINGS=NO
+DOXY_LOCATE=$(wildcard $(DIR_HDR)$1.hpp $(DIR_HDR)$1.tpp $(DIR_SRC)$1.cpp $(DIR_APP)$1.cpp)
 
-override DOXY_INPUT+=$(DOXY_USE_MDFILE_AS_MAINPAGE) $(SRCS_EXE) $(SRCS_SO) \
-	$(call WPAT,HPP,*) $(call WPAT,TPP,*)
+doxy-reset:
+	@$(doxymake) $(FILE_DOC) >$(NULL)
+	$(eval DOXY_VARS:=HTML_EXTRA_STYLESHEET BUILTIN_STL_SUPPORT COLS_IN_ALPHA_INDEX \
+		DOT_TRANSPARENT EXCLUDE_PATTERNS EXTRACT_ALL EXTRACT_PRIVATE EXTRACT_PACKAGE INPUT \
+		OUTPUT_DIRECTORY QT_AUTOBRIEF MULTILINE_CPP_IS_BRIEF RECURSIVE SHOW_INCLUDE_FILES \
+		STRIP_FROM_PATH WARNINGS \
+		$(foreach G,HTML LATEX RTF MAN,$(eval GENERATE_$G=NO)GENERATE_$G))
 
-V_DOXY_ALL=$(filter DOXY_%,$(.VARIABLES))
-V_DOXY_ALL_SUFFIXES=$(V_DOXY_ALL:DOXY_%=%)
-V_DOXY_REPLACEMENT=$(foreach V,$(V_DOXY_ALL_SUFFIXES),\\n$(V) = $(DOXY_$(V)))
+doxy-tag-% $(DIR_TAG)%.tag: doxy-reset $(call DOXY_LOCATE,$*)
+	@$(doxymake) $(FILE_DOC) >$(NULL)
+	@echo $(foreach VAR,$(DOXY_VARS),"$(VAR)=$($(VAR))\n") "GENERATE_TAGFILE=$(DIR_TAG)$*.tag" \
+		>>$(FILE_DOC)
+	@$(doxygen) $(FILE_DOC) >$(NULL)
 
-$(FILE_DOC): Doxygen.mk $(DOXY_INPUT)
-	@$(doxymake) $(FILE_DOC) >/dev/null && \
-		echo $(strip $(V_DOXY_REPLACEMENT)) >> $(FILE_DOC)
+$(FILE_DOC): Doxygen.mk $(wildcard $(DOXY_DISCOVER))
+	@$(doxymake) $(FILE_DOC) >$(NULL)
 
-doc: Doxygen.mk $(FILE_DOC)
-	@$(doxygen) $(FILE_DOC) >/dev/null
+doxy-tags: doxy-reset $(NAMES_DOC)
+
+#doc: doxy-tags doxy-reset
+#	@echo $(foreach VAR,$(DOXY_VARS),"$(VAR)=$($(VAR))\n") \
+#		"GENERATE_HTML=YES\n" "GENERATE_LATEX=YES\n" "GENERATE_TAGFILE=\n" \
+#		"TAGFILES=$(patsubst %,%=$(DIR_DOC)html/,$(wildcard $(DIR_TAG)*))" >>$(FILE_DOC)
+#	@$(doxygen) $(FILE_DOC) >$(NULL)
+doc: doxy-reset
+	@echo $(foreach VAR,$(DOXY_VARS),"$(VAR)=$($(VAR))\n") \
+		"GENERATE_HTML=YES\n" "GENERATE_RTF=YES\n" "GENERATE_LATEX=YES\n" >>$(FILE_DOC)
+	@$(doxygen) $(FILE_DOC) >$(NULL)
+
 clean-doc:; $(RM) $(FILE_DOC)
-.PHONY: doc clean-doc
+.PHONY: doc doxy-reset doxy-tag doxy-tags doxy-tag-% clean-doc
