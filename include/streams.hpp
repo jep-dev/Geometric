a77d54d6e914eada504c36a3cd826f7546e9f473
John Petersen <jepetersen.dev@gmail.com>
2018-08-01 23:37:45 -0400

    Added a common 'streams' file to aggregate common 'using' statements; may include files that include it (instead?)

---
+++ include/streams.hpp
@@ -0,0 +1,17 @@
+#ifndef STREAMS_HPP
+#define STREAMS_HPP
+
+#include <iosfwd>
+#include <string>
+#include <vector>
+
+#include "utilities.hpp"
+
+namespace Streams {
+using std::string;
+using std::vector;
+using std::ostringstream;
+using std::istringstream;
+}
+
+#endif
