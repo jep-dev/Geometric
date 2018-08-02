e2de71cc5e6c01a3ef6220d940430578b44708d7
John Petersen <jepetersen.dev@gmail.com>
2018-08-02 03:27:58 -0400

    Updated to use the Stopwatch type added in previous commit

--- app/performance.cpp
+++ app/performance.cpp
@@ -26,10 +26,15 @@ int main(int argc, const char *argv[]) {
 
 	// using T = float;
 	using T = double;
-	unsigned M = 10000, N = M * 100;
+	int M = 256, N = 2;
 	int n_digits = max(numDigits(M), numDigits(N));
 
+	Stopwatch<> stopwatch;
 	auto flags = cout.flags();
-	cout << "Quaternion<T>:\n" << delta(Quaternion<T>{1}, 100, 1000, 10000) << endl;
-	cout << "DualQuaternion<T>:\n" << delta(DualQuaternion<T>{1}, 100, 1000, 10000) << endl;
+	cout << "Quaternion<T>:\n" << delta(Quaternion<T>{1}, M, M*N, M*N*N, M*N*N*N) << endl;
+	cout << "  Elapsed (total): " << stopwatch.update().count() << " seconds" << endl;
+	stopwatch.advance();
+	cout << "DualQuaternion<T>:\n" << delta(DualQuaternion<T>{1}, M, M*N, M*N*N, M*N*N*N) << endl;
+	cout << "  Elapsed (total): " << stopwatch.update().count() << " seconds" << endl;
+	cout.setf(flags);
 }
