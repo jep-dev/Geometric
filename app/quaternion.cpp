7c543834298b14a3f3134013c8de9009f9e4b6d1
John Petersen <jepetersen.dev@gmail.com>
2018-08-04 18:44:47 -0400

    Added print loops for length, division, lerp, and nlerp

--- app/quaternion.cpp
+++ app/quaternion.cpp
@@ -6,51 +6,60 @@
 int main(int argc, const char *argv[]) {
 	using namespace std;
 
-
 	using T = float;
 	unsigned N = 2;
-	T angle = M_PI / 2, scale = 1, scale2 = sqrt(2);
+	T angle = M_PI / 2, scale = 2, scale2 = sqrt(2);
 	string bord(N, ' '), sep(1, ' '), pad(N, ' '),
 			tbord(N, ' '), tsep(sep.length(), '_'), tpad(N, ' ');
-	bord[1] = /*tbord[1] =*/ '|';
+	tpad[1] = 'x';
+	bord[1] = '|';
+
 	Quaternion<T> LHS[] = {{1}, {0,1}, {0,0,1}, {0,0,0,1}},
-		RHS[] = {rotation(angle, 1, 0, 0) * scale,
-				rotation(angle, 0, 1, 0) * scale, rotation(angle, 0, 0, 1) * scale};
+		RHS[] = {scale * rotation(angle, 1, 0, 0),
+				scale * rotation(angle, 0, 1, 0),
+				scale * rotation(angle, 0, 0, 1)};
 
-	/*static constexpr const char *epsilon = "\u03B5";
-	cout << "Near:\n";
-	auto nl = std::numeric_limits<T>::epsilon();
-	T lvals[] = {0, nl, 2*nl, 20*nl}, rvals[] = {0, nl, nl*10, nl*100};
-	for(T lhs : lvals) {
-		for(T rhs : rvals) {
-			cout << lhs/nl << "*" << epsilon << ' ' << (near_zero(lhs, rhs) ? "is" : "isn't")
-					<< " within " << rhs/nl << "*" << epsilon << " of 0\n  "
-					"and " << (near(lhs, rhs, nl) ? "is" : "isn't")
-					<< " within " << epsilon << " of " << rhs/nl << "*" << epsilon << endl;
-		}
-	}
-	return 0;*/
+	auto fills = setfill(' '), fillu = setfill('_');
+	auto setn = setw(N);
 
-	cout << ' ' << tpad << tbord << setfill('_');
+	cout << tpad << tbord << fillu;
 	for(auto const& lhs : LHS)
 		cout << setw(N) << lhs << tsep;
 	long i = 0, iMax = sizeof(LHS) / sizeof(LHS[0]);
-	cout << ' ' << setfill(' ') << endl;
+	cout << fills << endl;
 	for(auto const& lhs : LHS) {
-		cout << ' ' << setw(N) << lhs << bord;
-		if(i == (iMax-1)) cout << setfill('_');
+		cout << setw(N) << lhs << bord;
+		if(i == (iMax-1)) cout << fillu;
 		for(auto const& rhs : LHS)
 			cout << setw(N) << (lhs * rhs) << ((i == (iMax - 1)) ? tsep : sep);
 		cout << '|' << endl;
 		i++;
-		cout << setfill(' ');
+		cout << fills;
 	}
 	endl(cout);
 	for(auto const& lhs : LHS) {
 		for(auto const& rhs : RHS) {
-			cout << '(' << (rhs*sqrtf(2)) << ")/\u221A2 * " << lhs
-					<< " * (" << (*rhs*sqrtf(2)) << ")/\u221A2 = "
-					<< setw(3) << right << (rhs * lhs * * rhs) << endl;
+			cout << '(' << ((scale2/scale)*rhs) << ")*" << scale << "/\u221A2 * " << lhs
+					<< " * (" << (*rhs*(scale2/scale)) << ")*" << scale << "/\u221A2 = "
+					<< setw(numDigits(scale)+2) << right << (rhs * lhs * * rhs) << endl;
+		}
+	}
+	for(auto const& rhs : RHS) {
+		auto scaled = scale2 / scale * rhs;
+		ostringstream oss1, oss2, oss3;
+		oss1 << scaled, oss2 << scale << "/\u221A2";
+		auto str1 = oss1.str(), str2 = oss2.str(), str3 = str1 + "*" + str2;
+		cout << "For u = " << str1 << "...\n"
+			"   u/u  = " << rhs/rhs << ";\n"
+			"  u/|u| = " << rhs.normalize() << ";\n"
+			"   1/u  = " << 1/rhs << "\n";
+	}
+	{
+		auto const& lhs = LHS[0], rhs = LHS[1];
+		cout << "For u = " << lhs << ", v = " << rhs << "...\n";
+		for(float t = 0; t < 1 + .1 - std::numeric_limits<float>::epsilon(); t += .1) {
+			cout << "  lerp(u, v, " << t << ") = " << lerp(lhs, rhs, t) << "; "
+			"nlerp(u, v, " << t << ") = " << nlerp(lhs, rhs, t) << endl;
 		}
 	}
 }
