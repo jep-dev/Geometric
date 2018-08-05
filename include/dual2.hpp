40cc3fcf23c801adfcfc4a05e737a0236466f1d9
John Petersen <jepetersen.dev@gmail.com>
2018-08-05 13:07:34 -0400

    Added sclerp, operator* with scalars and other dual quaternions, and a macro indicating that dual2 was used to implement dual quaternions

--- include/dual2.hpp
+++ include/dual2.hpp
@@ -1,9 +1,13 @@
 #ifndef DUAL2_HPP
-#define DUAL2_HPP
+#define DUAL2_HPP "dual2.hpp"
+#define DUAL_IMPL DUAL2_HPP
 
 #include "quaternion.tpp"
 
 template<class S> struct DualQuaternion {
+private:
+	template<class... U> using Com = std::common_type_t<U...>;
+public:
 	Quaternion<S> u, v;
 	S& operator[](unsigned i) {
 		switch(i) {
@@ -32,27 +36,42 @@ template<class S> struct DualQuaternion {
 	DualQuaternion operator-(void) const { return {-u, -v}; }
 	DualQuaternion operator*(void) const { return {*u, {-v.w, v.x, v.y, v.z}}; }
 	template<class T>
-	DualQuaternion<std::common_type_t<S,T>> operator+(DualQuaternion const& rhs) const
-		{ return {u+rhs.u, v+rhs.v}; }
+	auto operator+(DualQuaternion<T> const& rhs)
+		-> DualQuaternion<std::common_type_t<S,T>> const { return {u+rhs.u, v+rhs.v}; }
 	template<class T>
-	DualQuaternion<std::common_type_t<S,T>> operator-(DualQuaternion const& rhs) const
-		{ return {u-rhs.u, v-rhs.v}; }
+	auto operator-(DualQuaternion<T> const& rhs)
+		-> DualQuaternion<std::common_type_t<S,T>> const { return {u-rhs.u, v-rhs.v}; }
 	template<class T>
-	DualQuaternion<std::common_type_t<S,T>> operator*(DualQuaternion<T> const& rhs) const
-		{ return {u*rhs.u, u*rhs.v + v*rhs.u}; }
+	auto operator*(DualQuaternion<T> const& rhs)
+		-> DualQuaternion<std::common_type_t<S,T>> const { return {u*rhs.u, u*rhs.v + v*rhs.u}; }
+	template<class C>
+	auto operator*(C const& c)
+		-> DualQuaternion<std::common_type_t<S,C>> const { return {c*u, c*v}; }
+	template<class C>
+	friend auto operator*(DualQuaternion<C> const& lhs, DualQuaternion<S> const& rhs)
+		-> DualQuaternion<std::common_type_t<S,C>>
+			{ return {lhs.u*rhs.u, lhs.u*rhs.v + lhs.v*rhs.u}; }
+	template<class C>
+	friend auto operator*(C const& c, DualQuaternion const& d)
+		-> DualQuaternion<std::common_type_t<S,C>> { return {c*d.u, c*d.v}; }
 	template<class T>
-	DualQuaternion<std::common_type_t<S,T>> operator^(DualQuaternion<T> const& rhs) const
-		{ return {u * rhs.u * *u, u * rhs.v * *u}; }
+	auto operator^(DualQuaternion<T> const& rhs)
+		-> DualQuaternion<std::common_type_t<S,T>> const
+			{ return {u * rhs.u * *u, u * rhs.v * *u}; }
 	template<class T>
-	Quaternion<std::common_type_t<S,T>> operator^(Quaternion<T> const& rhs) const
-		{ return {u * rhs * *u, v * rhs * *u - u * rhs * *v}; }
+	auto operator^(Quaternion<T> const& rhs) -> Quaternion<std::common_type_t<S,T>> const
+			{ return {u * rhs * *u, v * rhs * *u - u * rhs * *v}; }
+	bool operator==(DualQuaternion const& d) const { return u == d.u && v == d.v; }
 	DualQuaternion(S p, S q = 0, S r = 0, S s = 0, S w = 0, S x = 0, S y = 0, S z = 0):
 		u{p, q, r, s}, v{w, x, y, z} {}
 	DualQuaternion(Quaternion<S> u = {0}, Quaternion<S> v = {0}): u(u), v(v) {}
 
 };
 
-/*template<class S, class T>
-T& operator<<(T&, DualQuaternion<S> const&);*/
+template<class L, class R, class T>
+auto sclerp(DualQuaternion<L> const& lhs, DualQuaternion<R> const& rhs, T && t)
+		-> DualQuaternion<std::common_type_t<L,R,T>> {
+	return {slerp(lhs.u, rhs.u, t), lerp(lhs.v, rhs.v, t)};
+}
 
 #endif
