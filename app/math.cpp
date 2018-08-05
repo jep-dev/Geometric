181b44794ecc79391f08e252e7823a87f07d4118
John Petersen <jepetersen.dev@gmail.com>
2018-08-05 09:10:47 -0400

    Generated 'setw' statements from decomposition of floats (requires that the decimals are a power of 2)

--- app/math.cpp
+++ app/math.cpp
@@ -1,28 +1,42 @@
 #include "math.hpp"
 #include <iostream>
 #include <iomanip>
+#include <vector>
 
 int main(int argc, const char *argv[]) {
 	using namespace std;
 
 	typedef float T;
+
 	static constexpr const char *epsilon = "\u03B5";
 	cout << "Proximity:\n";
-	T nl = .1;
-	T lvals[] = {0, .5, 1, 1.5, 2, 2.5, 3, 3.5, 4}, rvals[] = {1, 2};
-	auto N = 3;
-	cout << right << setw(N) << "" << "  ";
-	for(T const& rhs : lvals)
-		cout << setw(N) << rhs << " ";
+	T nl = .1, dl = .125, dr = dl,
+			adl = abs(dl), adr = abs(dr),
+			adlMaj = floor(adl), adrMaj = floor(adr),
+			adlMin = adl - adlMaj, adrMin = adr - adrMaj;
+	unsigned lmax = 8, rmax = ceil(lmax * dl / dr),
+			numDigitsL = ceil(log10(adlMaj+1)+1),
+			numDigitsR = ceil(log10(adrMaj+1)+1);
+	if(adlMin > 0) numDigitsL += 1 + floor(abs(log2(adlMin)));
+	if(adrMin > 0) numDigitsR += 1 + floor(abs(log2(adrMin)));
+	if(dl < 0) numDigitsL++;
+	if(dr < 0) numDigitsR++;
+	auto setL = setw(numDigitsL), setR = setw(numDigitsR);
+
+	vector<T> lvals, rvals;
+	for(auto l = 0; l <= lmax; l++)
+		lvals.emplace_back(l*dl);
+	for(auto r = 0; r <= rmax; r++)
+		rvals.emplace_back(r*dr);
+	cout << right << setL << "" << "  ";
+	for(T const& rhs : rvals)
+		cout << setR << rhs << " ";
 	cout << endl;
 	for(T const& lhs : lvals) {
-		cout << setw(N) << lhs << ": ";
-		for(T const& rhs : lvals) {
-			cout << setw(N) << near(lhs, rhs, nl) << " ";
+		cout << setL << lhs << ": ";
+		for(T const& rhs : rvals) {
+			cout << setR << near(lhs, rhs, nl) << " ";
 		}
-		/*cout << "  |  ";
-		for(T const& rhs : lvals) {
-		}*/
 		cout << endl;
 	}
 	return 0;
