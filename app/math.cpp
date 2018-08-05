0fdc8970a585c025713c5f30d584006fdcffa447
John Petersen <jepetersen.dev@gmail.com>
2018-08-05 02:19:34 -0400

    Added test app for common math utilities

---
+++ app/math.cpp
@@ -0,0 +1,26 @@
+#include "math.hpp"
+#include <iostream>
+#include <iomanip>
+
+int main(int argc, const char *argv[]) {
+	using namespace std;
+
+	typedef float T;
+	static constexpr const char *epsilon = "\u03B5";
+	cout << "Proximity:\n";
+	T nl = .5;
+	T lvals[] = {0, .5, 1, 1.5, 2}, rvals[] = {1, 2};
+	auto N = 3;
+	cout << right << setw(N) << "" << "  ";
+	for(T const& rhs : lvals)
+		cout << setw(N) << rhs << " ";
+	cout << endl;
+	for(T const& lhs : lvals) {
+		cout << setw(N) << lhs << ": ";
+		for(T const& rhs : lvals) {
+			cout << setw(N) << near(lhs, rhs, nl) << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
