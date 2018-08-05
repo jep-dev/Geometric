66781fc12482243f59ea9b8c5ad2eedfe465708a
John Petersen <jepetersen.dev@gmail.com>
2018-08-05 02:20:40 -0400

    Added sclerps to dual test (spherical-linear interpolation)

--- app/dual.cpp
+++ app/dual.cpp
@@ -9,12 +9,29 @@
 #include <iomanip>
 
 int main(int argc, const char *argv[]) {
-	DualQuaternion<float> LHS[] = {{1}, {0,1}, {0,0,1}, {0,0,0,1},
+	using namespace std;
+
+	typedef float T;
+	DualQuaternion<T> LHS[] = {{1}, {0,1}, {0,0,1}, {0,0,0,1},
 			{0,0,0,0,1}, {0,0,0,0,0,1}, {0,0,0,0,0,0,1}, {0,0,0,0,0,0,0,1}};
 	for(auto const& lhs : LHS) {
 		for(auto const& rhs : LHS) {
-			std::cout << std::setw(3) << (lhs * rhs) << " ";
+			cout << setw(3) << (lhs * rhs) << " ";
 		}
-		endl(std::cout);
+		endl(cout);
+	}
+	{
+		auto lhs = LHS[1], rhs = LHS[2] + 10 * LHS[6];
+		cout << "For u = " << lhs << ", v = " << rhs << "...\n";
+		for(T i = 0, di = .1, dj = di/5;
+				i < 1 + di - std::numeric_limits<T>::epsilon(); i += di) {
+			auto s = sclerp(lhs, rhs, i);
+			/*for(auto j = 0; j < 8; j++) {
+				if(near_zero(s[j], dj)) s[j] = 0;
+				else if(near(s[j], 1, dj)) s[j] = 1;
+				else if(near(s[j], -1, dj)) s[j] = -1;
+			}*/
+			cout << "  s = sclerp(u, v, " << i << ") = " << s << ";\n";
+		}
 	}
 }
