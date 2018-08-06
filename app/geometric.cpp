d0dcb60a27e25f57ebfc4af763dd2f08905497a0
John Petersen <jepetersen.dev@gmail.com>
2018-08-06 01:10:21 -0400

    Replaced hard-coded tabs with 'para' (for the end of a 'paragraph', normally line break + indent); added substitutions to condense some of the outputs; hoping to format output better than hackish search-and-replace

--- app/geometric.cpp
+++ app/geometric.cpp
@@ -52,30 +52,34 @@ int main(int argc, const char *argv[]) {
 		using XY = Tag<x, y>;
 		using YZ = Tag<y, z>;
 		using XYZ = Tag<x, y, z>;
+	using F = Tag<float>;
+	using D = Tag<double>;
+
+	string para = "\n  ";
 
 	std::ostringstream oss;
-	oss << "Basic types:\n\t"
-			"0 = " << make_pretty(E{}) << ";\n\t"
-			"U = " << make_pretty(U{}) << "; "
-			"V = " << make_pretty(V{}) << "; "
-			"W = " << make_pretty(W{}) << ";\n\t"
-			"X = " << make_pretty(X{}) << "; "
-			"Y = " << make_pretty(Y{}) << "; "
-			"Z = " << make_pretty(Z{}) << ".\n";
+	oss << "Basic types: " << para
+			<< "0 = " << make_pretty(E{}) << ";" << para
+			<< "U = " << make_pretty(U{}) << "; "
+			<< "V = " << make_pretty(V{}) << "; "
+			<< "W = " << make_pretty(W{}) << ";" << para
+			<< "X = " << make_pretty(X{}) << "; "
+			<< "Y = " << make_pretty(Y{}) << "; "
+			<< "Z = " << make_pretty(Z{}) << ".\n";
 
 	static_assert(std::is_same<decltype(E{}+E{}), E>::value
 			&& std::is_same<decltype(E{}+X{}), X>::value
 			&& std::is_same<decltype(U{}+E{}), U>::value,
 		"The empty tag must be the additive identity");
-	oss << "Direct sums (operator+):\n\t"
-			"0+0 = " << make_pretty(E{}+E{}) << "; "
-			"U+0 = " << make_pretty(U{}+E{}) << "; "
-			"0+X = " << make_pretty(E{}+X{}) << ";\n\t";
+	oss << "Direct sums (operator+):" << para
+			<< "0+0 = " << make_pretty(E{}+E{}) << "; "
+			<< "U+0 = " << make_pretty(U{}+E{}) << "; "
+			<< "0+X = " << make_pretty(E{}+X{}) << ";" << para;
 
 	static_assert(std::is_same<decltype(UV{}+W{}), decltype(U{}+VW{})>::value
 			&& std::is_same<decltype(XY{}+Z{}), decltype(X{}+YZ{})>::value,
 		"Operator+ must be associative");
-	oss << "(U+V)+W = U+(V+W) = " << make_pretty((U{}+V{})+W{}) << " (associative.)\n\t";
+	oss << "(U+V)+W = U+(V+W) = " << make_pretty((U{}+V{})+W{}) << " (associative.)" << para;
 
 	static_assert(std::is_same<decltype(V{}+U{}), decltype(reverse(UV{}))>::value,
 		"Operator+ must preserve order (non-commutative)");
@@ -89,11 +93,11 @@ int main(int argc, const char *argv[]) {
 			&& std::is_same<YZ, decltype(Y{}+Z{})>::value
 			&& std::is_same<XYZ, decltype(X{}+Y{}+Z{})>::value,
 		"Operator+ must be direct sum of types (treated as orthogonal)");
-	oss << "Inner product (operator*):\n\t"
-			"0*0 = " << make_pretty(E{}*E{}) << ";\n\t"
-			"U*X = " << make_pretty(U{}*X{}) << ";\n\t"
-			"(U+V) * (X+Y) = " << make_pretty(UV{}) << " * " << make_pretty(XY{})
-				<< " = " << make_pretty(UV{}*XY{}) << ";\n\t";
+	oss << "Inner product (operator*):" << para
+			<< "0*0 = " << make_pretty(E{}*E{}) << ";" << para
+			<< "U*X = " << make_pretty(U{}*X{}) << ";" << para
+			<< "(U+V) * (X+Y) = " << make_pretty(UV{}) << " * " << make_pretty(XY{})
+				<< " = " << make_pretty(UV{}*XY{}) << ";" << para;
 	static_assert(std::is_same<decltype(U{}*X{}+V{}*Y{}), decltype(UV{}*XY{})>::value,
 		"Operator* must be distributive over operator+");
 	oss << "U*X+V*Y = (U+V)*(X+Y) = " << make_pretty(U{}*X{}+V{}*Y{}) << ".\n";
@@ -102,40 +106,40 @@ int main(int argc, const char *argv[]) {
 			&& std::is_same<decltype(E{}^X{}), E>::value
 			&& std::is_same<decltype(U{}^E{}), E>::value,
 		"The empty tag must be the multiplicative zero");
-	oss << "Outer product (operator^):\n\t"
-			"0 ^ X = " << make_pretty(E{}^X{}) << ";\n\t"
-			"U ^ 0 = " << make_pretty(U{}^E{}) << ";\n\t"
-			"U ^ X = " << make_pretty(U{}^X{}) << ";\n\t"
-			"(U+V) ^ X = " << make_pretty(UV{} ^ X{}) << ";\n\t"
-			"U ^ (X+Y) = " << make_pretty(U{} ^ XY{}) << ";\n\t"
-			"(U+V) ^ (X+Y) = " << make_pretty(UV{} ^ XY{}) << ".\n\t"
-			"(U+V+W) ^ (X+Y) = " << make_pretty(UVW{} ^ XY{}) << ".\n\t"
-			"(U+V) ^ (X+Y+Z) = " << make_pretty(UV{} ^ XYZ{}) << ".\n\t"
-			"(U+V+W) ^ (X+Y+Z) = " << make_pretty(UVW{} ^ XYZ{}) << ".\n"
-		"Evaluation:\n\t"
-			"[0] = " << make_pretty(eval(E{})) << ";\n\t"
-			"[U] = " << make_pretty(eval(U{})) << ";\n\t"
-			"[U+V] = " << make_pretty(eval(UV{})) << ";\n\t"
-			"[(U+V)^(X+Y)] = " << make_pretty(eval(UV{}^XY{})) << ".\n"
-		"Outer product, transposed:\n\t"
-			"0 % X = " << make_pretty(E{} % X{}) << ";\n\t"
-			"U % 0 = " << make_pretty(U{} % E{}) << ";\n\t"
-			"U % X = " << make_pretty(U{} % X{}) << ";\n\t"
-			"(U+V) % X = " << make_pretty((UV{}) % X{}) << ";\n\t"
-			"U % (X+Y) = " << make_pretty(U{} % XY{}) << ";\n\t"
-			"(U+V) % (X+Y) = " << make_pretty(UV{} % XY{}) << ";\n\t"
-			"(U+V+W) % (X+Y+Z) = " << make_pretty(UVW{} % XYZ{}) << ".\n";
+	oss << "Outer product (operator^):" << para
+			<< "0 ^ X = " << make_pretty(E{}^X{}) << ";" << para
+			<< "U ^ 0 = " << make_pretty(U{}^E{}) << ";" << para
+			<< "U ^ X = " << make_pretty(U{}^X{}) << ";" << para
+			<< "(U+V) ^ X = " << make_pretty(UV{} ^ X{}) << ";" << para
+			<< "U ^ (X+Y) = " << make_pretty(U{} ^ XY{}) << ";" << para
+			<< "(U+V) ^ (X+Y) = " << make_pretty(UV{} ^ XY{}) << "." << para
+			<< "(U+V+W) ^ (X+Y) = " << make_pretty(UVW{} ^ XY{}) << "." << para
+			<< "(U+V) ^ (X+Y+Z) = " << make_pretty(UV{} ^ XYZ{}) << "." << para
+			<< "(U+V+W) ^ (X+Y+Z) = " << make_pretty(UVW{} ^ XYZ{}) << ".\n"
+		"Evaluation:" << para
+			<< "[0] = " << make_pretty(eval(E{})) << ";" << para
+			<< "[U] = " << make_pretty(eval(U{})) << ";" << para
+			<< "[U+V] = " << make_pretty(eval(UV{})) << ";" << para
+			<< "[(U+V)^(X+Y)] = " << make_pretty(eval(UV{}^XY{})) << ".\n"
+		"Outer product, transposed:" << para
+			<< "0 % X = " << make_pretty(E{} % X{}) << ";" << para
+			<< "U % 0 = " << make_pretty(U{} % E{}) << ";" << para
+			<< "U % X = " << make_pretty(U{} % X{}) << ";" << para
+			<< "(U+V) % X = " << make_pretty((UV{}) % X{}) << ";" << para
+			<< "U % (X+Y) = " << make_pretty(U{} % XY{}) << ";" << para
+			<< "(U+V) % (X+Y) = " << make_pretty(UV{} % XY{}) << ";" << para
+			<< "(U+V+W) % (X+Y+Z) = " << make_pretty(UVW{} % XYZ{}) << ".\n";
 	auto uv = eval(UV{});
 	static_assert(std::is_same<decltype(access<0>(uv)), u&>::value
 			&& std::is_same<decltype(access<1>(uv)), v&>::value,
 		"Access<I> must emulate std::get<I>");
-	oss << "Access:\n\t"
-			"[U+V]_0 = " << make_pretty(access<0>(eval(UV{}))) << ";\n\t"
-			"[U+V]_1 = " << make_pretty(access<1>(eval(UV{}))) << ";\n\t"
-			"[(U+V)^(X+Y)]_1 = " << make_pretty(access<1>(eval(UV{}^XY{}))) << ";\n\t"
-			"[(U+V)^(X+Y)]_1_1 = " << make_pretty(access<1,1>(eval(UV{}^XY{}))) << ";\n\t"
-			"[U+V]_0 . 1 = " << (access<0>(uv) = 1) << ";\n\t"
-			"[U+V]_1 . 2 = " << (access<1>(uv) = 2) << ".\n";
+	oss << "Access:" << para
+			<< "[U+V]_0 = " << make_pretty(access<0>(eval(UV{}))) << ";" << para
+			<< "[U+V]_1 = " << make_pretty(access<1>(eval(UV{}))) << ";" << para
+			<< "[(U+V)^(X+Y)]_1 = " << make_pretty(access<1>(eval(UV{}^XY{}))) << ";" << para
+			<< "[(U+V)^(X+Y)]_1_1 = " << make_pretty(access<1,1>(eval(UV{}^XY{}))) << ";" << para
+			<< "[U+V]_0 . 1 = " << (access<0>(uv) = 1) << ";" << para
+			<< "[U+V]_1 . 2 = " << (access<1>(uv) = 2) << ".\n";
 
 	static_assert(std::is_same<decltype(reverse(UVW{})), decltype(W{}+V{}+U{})>::value,
 		"Reverse must distribute over addition");
@@ -144,27 +148,39 @@ int main(int argc, const char *argv[]) {
 			&& std::is_same<decltype(reverse(UV{}*XY{})),
 				decltype(reverse(UV{})*reverse(XY{}))>::value,
 		"Reverse must distribute over multiplication");
-	oss << "Reversal:\n\t"
-			"rev(U+V) = " << make_pretty(reverse(UV{})) << ";\n\t"
-			"rev(U+V+W) = " << make_pretty(reverse(UVW{})) << ";\n\t"
-			"rev((U+V)^(X+Y)) = " << make_pretty(reverse(UV{}^XY{})) << ".\n";
+	oss << "Reversal:" << para
+			<< "rev(U+V) = " << make_pretty(reverse(UV{})) << ";" << para
+			<< "rev(U+V+W) = " << make_pretty(reverse(UVW{})) << ";" << para
+			<< "rev((U+V)^(X+Y)) = " << make_pretty(reverse(UV{}^XY{})) << ".\n";
+	oss << "Collapse:" << para
+			<< "collapse(0) = " << make_pretty(collapse(E{})) << ";" << para
+			<< "collapse(F) = " << make_pretty(collapse(F{})) << ";" << para
+			<< "collapse(F+D) = " << make_pretty(collapse(F{}+D{})) << ";" << para
+			<< "collapse((F+D)^(D+F)) = " << make_pretty(collapse((F{}+D{})^(D{}+F{}))) << ";" << para
+			<< "collapse({(F+D)^(D+F)}) = " << make_pretty(collapse(
+					Tag<decltype((F{}+D{})^(D{}+F{}))>{})) << ".\n";
+	oss << "Deval:" << para
+			<< "deval(" << make_pretty(eval(E{})) << ") = " << make_pretty(deval(eval(E{}))) << ".\n";
 	static_assert(std::is_same<decltype(reverse(UV{})), decltype(rotate(UV{}))>::value,
 		"Rotate must move the head type to the end of the tail");
 	static_assert(std::is_same<UV, decltype(rotate(rotate(UV{})))>::value,
 		"N rotations of N elements must be the identity");
-	oss << "Rotation:\n\t"
-			"rot(U+V+W) = " << make_pretty(rotate(UVW{})) << ";\n\t"
-			"rot^2(U+V+W) = " << make_pretty(rotate(rotate(UVW{}))) << ";\n\t"
-			"rot^3(U+V+W) = " << make_pretty(rotate(rotate(rotate(UVW{})))) << ".\n"
-		"Sequences:\n\t"
-			"seq(1,2) = " << make_pretty(Seq<1,2>{}) << ".";
+	oss << "Rotation:" << para
+			<< "rot(U+V+W) = " << make_pretty(rotate(UVW{})) << ";" << para
+			<< "rot^2(U+V+W) = " << make_pretty(rotate(rotate(UVW{}))) << ";" << para
+			<< "rot^3(U+V+W) = " << make_pretty(rotate(rotate(rotate(UVW{})))) << ".\n"
+		"Sequences:" << para
+			<< "seq(1,2) = " << make_pretty(Seq<1,2>{}) << ".";
 
 	auto res = oss.str();
 
 	using S2 = array<string, 2>;
 	for(auto const& it : vector<S2>{{"->", "-%"}, {"Detail::", ""}, {"Tag", ""},
-			{"std::tuple", "Val"}, {"std::integral_constant<unsigned int, ", "Const<"},
-			{"> >", ">>"}, {"<", "{"}, {">", "}"}, {"-%", "->"}}) {
+			{"std::tuple", "Val"}, {"float", "f"}, {"double", "d"},
+			{"std::integral_constant<unsigned int, ", "Const<"},
+			{"> >", ">>"}, {"<", "{"}, {">", "}"}, {"-%", "->"},
+			{"}, ", "},_"}, {", ", ","}, {"},_","}, "}
+			}) {
 		std::size_t pos;
 		while((pos = res.find(it[0])) != std::string::npos)
 			res.replace(pos, it[0].length(), it[1]);
