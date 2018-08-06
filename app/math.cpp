5acd172c402a93757ef4a4627ddc2274fbc1ab90
John Petersen <jepetersen.dev@gmail.com>
2018-08-06 03:29:44 -0400

    Replaced brittle floating point hacks with a less informative readout; added zeroes to the graph to emphasize edge cases of 'near'

--- app/math.cpp
+++ app/math.cpp
@@ -18,63 +18,51 @@ int main(int argc, const char *argv[]) {
 
 	typedef float T;
 
-	unsigned lmax = 32, rmax = 16;
-	T nl = .5f, dnl = .5f, nls[] = {nl, nl*dnl, nl*powf(dnl,2), nl*powf(dnl,3), nl*powf(dnl,4)},
-		dl = .25f, dr = .5f,
-		adl = abs(dl), adr = abs(dr),
-		adlMaj = floor(adl), adrMaj = floor(adr),
-		adlMin = adl - adlMaj, adrMin = adr - adrMaj,
-		adlMax = abs(dl*lmax), adrMax = abs(dr*rmax);
-	constexpr unsigned n_nls = sizeof(nls) / sizeof(nls[0]);
 	static constexpr const char *epsilon = "\u03B5";
+	//T nl = .0625f, dnl = sqrtf(2), nls[] = {nl, nl*dnl, nl*powf(dnl,2), nl*powf(dnl,3), nl*powf(dnl,4)};
+	T nl = .0625f, dnl = nl, nls[] = {nl, nl+dnl, nl+dnl*2, nl+dnl*3, nl+dnl*4};
+
+	static constexpr unsigned n_nls = sizeof(nls)/sizeof(nls[0]);
+	unsigned M = 16, N = 24,
+			nDigitsM = numDigits(M), nDigitsN = numDigits(N),
+			nTotal = N*(nDigitsN+1)+nDigitsM+1;
+	auto setM = setw(nDigitsM), setN = setw(nDigitsN),
+			setT = setw(nTotal), setShortT = setw(nTotal - nDigitsM - 1);
+
 	string shades[n_nls+1] = {"", "`", "'", "\"", "*", "#"};
-	auto shade_max = minimax(shades).second;
-	unsigned numDigitsNl = -log2(nls[n_nls-1]),
-			numDigitsL = numDigits(adlMax),
-			numDigitsR = numDigits(adrMax),
-			numDecimalsL = 0, numDecimalsR = 0;
-	if(adlMin > 0) {
-		numDecimalsL = floor(abs(log2(adlMin)));
-		numDigitsL += 1 + numDecimalsL;
-	}
-	if(adrMin > 0) {
-		numDecimalsR = floor(abs(log2(adrMin)));
-		numDigitsR += 1 + numDecimalsR;
-	}
-	if(dl < 0) numDigitsL++;
-	if(dr < 0) numDigitsR++;
-	auto numDigitsR0 = numDigitsR/2, numDigitsR1 = numDigitsR - numDigitsR0,
-			numDigitsL0 = numDigitsL/2, numDigitsL1 = numDigitsL - numDigitsL0;
-	auto setL = setw(numDigitsL), setL0 = setw(numDigitsL0), setL1 = setw(numDigitsL1),
-			setR = setw(numDigitsR), setR0 = setw(numDigitsR0), setR1 = setw(numDigitsR1);
+	auto interp = [=] (auto && i, auto && j) -> pair<T,T> {
+		// return {log(T(i)/N+1), T(j)/N}; // Log-linear scale
+		return {T(i)/M, T(j)/N}; // Linear scale
+	};
+	auto xformat = [=] (auto &s, auto x, bool lhs = false)
+		-> decltype(s) { return s << (lhs ? setN : setM) << x, s; };
+	auto hrule = [=] (auto &s) -> decltype(s) {
+		auto f = cout.fill();
+		s << setfill(' ') << setM << "" << '+'
+				<< setfill('-') << setShortT << "" << '+';
+		return s << setfill(f), s;
+	};
 
-	vector<T> lvals, rvals;
-	for(auto l = 0; l <= lmax; l++) lvals.emplace_back(l*dl);
-	for(auto r = 0; r <= rmax; r++) rvals.emplace_back(r*dr);
-
-	unsigned n_total = rvals.size() * (1+numDigitsR) + numDigitsL + 2;
-	center(cout << setfill('_'), "Floating point comparisons", n_total) << setfill(' ') << endl;
-	cout << "Legend: ";
-	int n_nls_digits = ceil(abs(log2(nls[n_nls-1])));
-	for(auto i = 0; i < n_nls+1; i++) {
-		cout << "[" << setw(shade_max) << shades[i] << "]=" << left;
-		if(i) cout << "1/2^" << setw(n_nls_digits-4) << ceil(abs(log2(nls[i-1])));
-		else cout << setw(n_nls_digits) << "n/a";
-		cout << "  ";
-	}
-	cout << "|\n" << setfill('-') << setw(n_total) << "" << "+\n" << setfill(' ');
-	cout << fixed << right << setprecision(numDecimalsR) << setL0 << "" << setL1 << "" << "  ";
-	for(T const& rhs : rvals) cout << setR << rhs << " ";
-	cout << '|' << endl;
-	for(T const& lhs : lvals) {
-		cout << setL << setprecision(numDecimalsL) << lhs << ": ";
-		for(T const& rhs : rvals) {
-			unsigned nears = 0;
-			for(auto const& n : nls)
-				if(near(lhs, rhs, n)) nears++;
-			center(cout, shades[nears], numDigitsR+1);
+	hrule(cout) << endl;
+	xformat(cout, "", true) << '|';
+	for(unsigned j = 0; j < N; j++)
+		xformat(cout, j) << ' ';
+	hrule(cout << '|' << endl) << endl;
+	for(unsigned i = 0; i < M; i++) {
+		xformat(cout, i, true) << '|';
+		for(unsigned j = 0; j < N; j++) {
+			auto uv = interp(i, j);
+			cout << right;
+			unsigned hits = 0;
+			for(auto const& lim : nls)
+				if(near(uv.first, uv.second, lim)) hits++;
+			if(hits) xformat(cout, shades[hits]) << ' ';
+			else if(near_zero(uv.first, nl) || near_zero(uv.second, nl))
+				xformat(cout, '0') << ' ';
+			else xformat(cout, shades[0]) << ' ';
 		}
-		cout << '|' << endl;
+		endl(cout << '|');
 	}
-	cout << setfill('-') << setw(n_total) << "" << "'" << endl;
+	hrule(cout) << endl;
+
 }
