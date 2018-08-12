8e7da388c360620d48ed5a96f0f520dff49ae130
John Petersen <jepetersen.dev@gmail.com>
2018-08-12 17:40:46 -0400

    Added image header and app; currently reads arg 2 and writes to arg 1, after verifying privilege, as a proof of concept

---
+++ app/image.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+#include "image.hpp"
+
+int main(int argc, const char *argv[]) {
+	//using namespace std;
+	//Image image = {128, 128};
+	if(argc > 2) {
+		std::fstream fs;
+		fs.open(argv[1], std::fstream::out);
+		if(fs.good()) {
+			fs.close();
+			fs.open(argv[2], std::fstream::in);
+			if(fs.good()) {
+				fs.close();
+				Image image(argv[2]);
+				image.write(argv[1]);
+			}
+		}
+	}
+}
---
+++ include/image.hpp
@@ -0,0 +1,8 @@
+#ifndef IMAGE_HPP
+#define IMAGE_HPP
+
+#include <png++/png.hpp>
+
+using Image = png::image<png::rgba_pixel>;
+
+#endif
