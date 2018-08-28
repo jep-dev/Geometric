031b145d1c9dfda36a96d4f96666e26366162a09
John Petersen <jepetersen.dev@gmail.com>
2018-08-28 18:42:13 -0400

    Began writing [dual] quaternion definitions in GLSL (to correspond to DualQuaternion as in dual.hpp); up to projection, all of the traditional matrix operations can be expressed in [dual] quaternions.

---
+++ share/dual.glsl
@@ -0,0 +1,33 @@
+#version 330
+
+struct Dual { vec4 u, v; };
+
+// Conjugate of a quaternion
+vec4 qconj(vec4 q) { return vec4(q.w, -q.xyz); }
+
+// Squared Euclidean norm (omits the square root in q.length())
+float qlen2(vec4 q) { return q.w*q.w + q.x*q.x + q.y*q.y + q.z*q.z; }
+
+// Quaternion product
+vec4 qmul(vec4 l, vec4 r) {
+	return vec4(
+		l.w*r.w - l.x*r.x - l.y*r.y - l.z*r.z,
+		l.w*r.x + l.x*r.w + l.y*r.z - l.z*r.y,
+		l.w*r.y - l.x*r.z + l.y*r.w + l.z*r.x,
+		l.w*r.z + l.x*r.y - l.y*r.x + l.z*r.w
+	);
+}
+
+// Quaternion division (pre-normalized denominator)
+vec4 qdivn(vec4 n, vec4 d) { return qmul(n, qconj(d)); }
+// Quaternion division (unrestricted denominator)
+vec4 qdiv(vec4 n, vec4 d) { return qdivn(n, d) / qlen2(d); }
+
+// Dual quaternion conjugate (combined dual conjugate and quaternion conjugate)
+Dual dconj(Dual d) { return Dual(qconj(d.u), -qconj(d.v)); }
+// Dual multiplication
+Dual dmul(Dual l, Dual r) { return Dual(qmul(l.u, r.u), qmul(l.v, r.u) + qmul(l.u, r.v)); }
+// Dual division by quaternion (pre-normalized denominator)
+Dual ddivn(Dual l, vec4 r) { return Dual(qdivn(l.u, r), qdivn(l.v, r)); }
+// Dual division by quaternion (unrestricted denominator)
+Dual ddiv(Dual l, vec4 r) { return Dual(qdiv(l.u, r), qdiv(l.v, r)); }
