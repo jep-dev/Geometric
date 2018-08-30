e62bd0796137e02d177f62190bfd9efa20f4f78c
John Petersen <jepetersen.dev@gmail.com>
2018-08-29 23:10:52 -0400

    Added 'ShaderTable' type to manage sources and objects both as a group and by given name rather than explicitly by variable name

--- include/shader.hpp
+++ include/shader.hpp
@@ -23,6 +23,7 @@ namespace Detail {
 	bool is_error(GLenum e);
 	bool get_error(void);
 	bool is_sourced(GLuint shader);
+	bool set_source(GLuint shader, std::string const& content);
 	bool source(GLuint shader, const char *filename);
 	bool is_compiled(GLuint shader);
 	bool compile(GLuint shader);
@@ -46,6 +47,7 @@ struct Shader {
 	Events::Status status;
 	operator GLuint(void) const;
 	bool source(const char *fname, bool force = true);
+	bool set_source(std::string const& value);
 	bool compile(bool force = true);
 	Shader(GLuint value);
 	Shader(GLuint value, const char *fname);
@@ -53,6 +55,7 @@ struct Shader {
 	Shader(const char *fname, GLenum type = GL_VERTEX_SHADER);
 	virtual ~Shader(void) { glDeleteShader(value); value = 0; }
 };
+
 struct Program {
 	GLuint value;
 	bool attached = false, linked = false;
@@ -71,6 +74,54 @@ struct Program {
 	Program(void);
 	virtual ~Program(void) { glDeleteProgram(value); value = 0; }
 };
+
+struct ShaderTable {
+	std::map<std::string, std::string> sources;
+	std::map<std::string, View::Shader> objects;
+
+	template<class U, class V>
+	bool add_source(U const& key, V const& value, bool needs_source = true) {
+		if(needs_source) {
+			std::string lines;
+			if(!Streams::readLines(value, lines))
+				return false;
+			sources.emplace(key, lines);
+		} else sources.emplace(key, value);
+		return true;
+	}
+	template<class U>
+	Events::Status add_shader(const gl::GLenum type, U const& key) {
+		auto emplaced = objects.emplace(key, type);
+		return emplaced.first -> status;
+	}
+	template<class U>
+	Events::Status compile_shader(U const& key) {
+		auto it = objects.find(key);
+		if(it == objects.end()) return false;
+		return it -> compile();
+	}
+	template<class U>
+	Events::Status attach(Program &program, U const& key) {
+		auto it = objects.find(key);
+		if(it == objects.end())
+			return { Events::StatusError };
+		if(!program.attach(*it))
+			// TODO use program.status after setting it from program.attach
+			return { Events::StatusError };
+		return {};
+	}
+	// TODO determine if attach order matters
+	Events::Status attach_all(Program &program) {
+		for(auto & it : objects) {
+			if(!program.attach(it.second))
+				// TODO see note in attach()
+				return { Events::StatusError };
+		}
+		return {};
+	}
+
+};
+
 
 }
 
