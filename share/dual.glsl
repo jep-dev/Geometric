bac44ad4ce50a2d31833325a7329a6227147a92b
John Petersen <jepetersen.dev@gmail.com>
2018-08-30 03:51:13 -0400

    Fixed several bugs caused by vectors having members 'xyzw' rather than 'wxyz'; added 'qdmul' and 'dqmul' for heterogeneous products, and similarly, 'qdsandwich' (rotation of dual)

--- share/dual.glsl
+++ share/dual.glsl
@@ -1,9 +1,9 @@
 #version 330
 
-struct Dual { vec4 u, v; };
+struct Dual { vec4 u; vec4 v; };
 
 // Conjugate of a quaternion
-vec4 qconj(vec4 q) { return vec4(q.w, -q.xyz); }
+vec4 qconj(vec4 q) { return vec4(q.x, -q.yzw); }
 
 // Squared Euclidean norm (omits the square root in q.length())
 float qlen2(vec4 q) { return q.w*q.w + q.x*q.x + q.y*q.y + q.z*q.z; }
@@ -11,10 +11,10 @@ float qlen2(vec4 q) { return q.w*q.w + q.x*q.x + q.y*q.y + q.z*q.z; }
 // Quaternion product
 vec4 qmul(vec4 l, vec4 r) {
 	return vec4(
-		l.w*r.w - l.x*r.x - l.y*r.y - l.z*r.z,
-		l.w*r.x + l.x*r.w + l.y*r.z - l.z*r.y,
-		l.w*r.y - l.x*r.z + l.y*r.w + l.z*r.x,
-		l.w*r.z + l.x*r.y - l.y*r.x + l.z*r.w
+		l.x*r.x - l.y*r.y - l.z*r.z - l.w*r.w,
+		l.x*r.y + l.y*r.x + l.z*r.w - l.w*r.z,
+		l.x*r.z - l.y*r.w + l.z*r.x + l.w*r.y,
+		l.x*r.w + l.y*r.z - l.z*r.y + l.w*r.x
 	);
 }
 
@@ -26,7 +26,7 @@ vec4 qdiv(vec4 n, vec4 d) { return qdivn(n, d) / qlen2(d); }
 vec4 qsandwich(vec4 u, vec4 v) { return qmul(qmul(u, v), qconj(u)); }
 
 // Dual quaternion conjugate (combined dual conjugate and quaternion conjugate)
-Dual dconj(Dual d) { return Dual(qconj(d.u), -qconj(d.v)); }
+Dual dconj(Dual d) { return Dual(qconj(d.u), vec4(-d.v.x, d.v.yzw)); }
 // Dual multiplication
 Dual dmul(Dual l, Dual r) { return Dual(qmul(l.u, r.u), qmul(l.v, r.u) + qmul(l.u, r.v)); }
 // Dual division by quaternion (pre-normalized denominator)
@@ -35,3 +35,8 @@ Dual ddivn(Dual l, vec4 r) { return Dual(qdivn(l.u, r), qdivn(l.v, r)); }
 Dual ddiv(Dual l, vec4 r) { return Dual(qdiv(l.u, r), qdiv(l.v, r)); }
 // Dual 'sandwich' product
 Dual dsandwich(Dual u, Dual v) { return dmul(dmul(u, v), dconj(u)); }
+
+Dual qdmul(vec4 l, Dual r) { return Dual(qmul(l, r.u), qmul(l, r.v)); }
+Dual dqmul(Dual l, vec4 r) { return Dual(qmul(l.u, r), qmul(l.v, r)); }
+
+Dual qdsandwich(vec4 l, Dual r) { return dqmul(qdmul(l, r), qconj(l)); }
