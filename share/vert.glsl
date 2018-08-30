cb9aeecd8e38e1a17911a0905ebefa819224f9cd
John Petersen <jepetersen.dev@gmail.com>
2018-08-30 06:17:57 -0400

    Resolved bug discovered in prev. commit; uniform default initialization is not guaranteed to behave like normal variable initialization, in this case default init of one to the default of another. Using a defaulted const variable as the value shared by other uniform defaults eliminates the discrepancy.

--- share/vert.glsl
+++ share/vert.glsl
@@ -16,15 +16,16 @@ Dual dqmul(Dual, vec4);
 Dual qdmul(vec4, Dual);
 Dual qdsandwich(vec4, Dual);
 
-uniform Dual model = Dual(vec4(cos(M_PI/8),0,0,sin(M_PI/8)), vec4(0,0,0.25,0)), view;
-uniform vec4 u = vec4(cos(M_PI/8),0,sin(M_PI/8),0), v;
+const vec4 model_u = vec4(cos(M_PI/8),0,sin(M_PI/8),0), model_v = vec4(0, 0, 0.25, 0);
+uniform vec4 u = model_u, v = model_v;
+uniform Dual model = Dual(model_u, model_v), view;
 
 in vec4 pos_in;
 out vec4 pos_out;
 
 void main(){
 	Dual x = Dual(vec4(1,0,0,0), vec4(0, pos_in.xyz));
-	Dual y = dsandwich(Dual(u, vec4(0,0,0.25,0)), x);
+	Dual y = dsandwich(Dual(u, v), x);
 	//Dual y = dsandwich(model, x);
 	gl_Position = projection * vec4(y.v.yzw, 1);
     pos_out = gl_Position;
