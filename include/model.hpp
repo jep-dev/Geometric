0c3ea2ef5b7839bc8024a31e67ed91521c8da11c
John Petersen <jepetersen.dev@gmail.com>
2018-09-02 17:09:10 -0400

    Added basic tree type model; transforms are accumulated from root to leaf, and leaves have vertices/indices; transforms are not applied to vertices because they're intended to be used as model/view transforms in shaders

--- include/model.hpp
+++ include/model.hpp
@@ -2,8 +2,47 @@
 #define MODEL_HPP
 
 #include "dual.hpp"
+#include <vector>
 
+
+template<class T>
+struct Model;
+template<class T>
 struct Model {
+	std::vector<Model<T>> children;
+	std::vector<T> vertices;
+	std::vector<unsigned> indices;
+	DualQuaternion<T> transform = {1}, accumulated = transform;
+	void accumulate(void) {
+		//accumulated = transform;
+		for(auto & c : children)
+			c.accumulate(accumulated);
+	}
+	void accumulate(DualQuaternion<T> const& d) {
+		accumulated = d * transform;
+		accumulate();
+	}
+	Model<T>& add_child(Model<T> && m = {}) {
+		children.emplace_back(std::move(m));
+		return children.back();
+	}
+	Model<T>& add_vertices(void) {
+		return *this;
+	}
+	template<class U, class... V>
+	Model<T>& add_vertices(U && u, V &&... v) {
+		vertices.emplace_back(std::forward<U>(u));
+		return add_vertices(std::forward<V>(v)...);
+	}
+	Model<T> add_indices(void) {
+		return *this;
+	}
+	template<class... V>
+	Model<T> add_indices(unsigned u, V &&... v) {
+		indices.emplace_back(u);
+		return add_indices(std::forward<V>(v)...);
+	}
 };
+
 
 #endif
