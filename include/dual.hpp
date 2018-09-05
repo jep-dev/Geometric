b9df02f8902553c08a60dd9921ad1e2c6b8134c1
John Petersen <jepetersen.dev@gmail.com>
2018-09-04 22:17:01 -0400

    Renamed type -> value_type; added assignment operators (addition, etc.); corrected a lot of signatures as in quaternion, better handling for heterogeneous types; disambiguated product signatures with some enable_ifs; added division; added sandwich operator as '^'; rewrote some of the *erp functions; removed some of the user-defined literals (moved to quaternion)

--- include/dual.hpp
+++ include/dual.hpp
@@ -8,13 +8,14 @@
 ///@endcond
 
 #include "math.hpp"
+#include "quaternion.hpp"
 #include "quaternion.tpp"
 
 template<class S> struct DualQuaternion;
 
 /** A dual quaternion with a flat structure. */
 template<class S> struct DualQuaternion {
-	using type = S;
+	using value_type = S;
 	S s, t, u, v, w, x, y, z;
 	/** Const member access by index and unit. */
 	const S& operator[](unsigned i) const {
@@ -43,11 +44,11 @@ template<class S> struct DualQuaternion {
 		}
 	}
 	/** Square, as in product with itself. */
-	type square(void) const { return *this * *this; }
+	value_type square(void) const { return *this * *this; }
 	/** Square of the norm, as in sum of squared elements. */
-	type lengthSquared(void) const { return s*s + t*t + u*u + v*v + w*w + x*x + y*y + z*z; }
+	value_type lengthSquared(void) const { return s*s + t*t + u*u + v*v + w*w + x*x + y*y + z*z; }
 	/** Euclidean norm. */
-	type length(void) const { return sqrt(lengthSquared()); }
+	value_type length(void) const { return sqrt(lengthSquared()); }
 	/** Conjugate; composed by the complex and dual conjugates. */
 	template<class T> DualQuaternion& operator=(DualQuaternion<T> const& d) {
 		s = d.s, t = d.t, u = d.u, v = d.v;
@@ -57,69 +58,78 @@ template<class S> struct DualQuaternion {
 
 	DualQuaternion operator*(void) const { return {s, -t, -u, -v, -w, x, y, z}; }
 	DualQuaternion operator-(void) const { return {-s, -t, -u, -v, -w, -x, -y, -z}; }
-	DualQuaternion operator*(DualQuaternion const& d) const {
-		return {
-			s*d.s - t*d.t - u*d.u - v*d.v,  s*d.t + t*d.s + u*d.v - v*d.u,
-			s*d.u + u*d.s - t*d.v + v*d.t,  s*d.v + v*d.s + t*d.u - u*d.t,
-			s*d.w - t*d.x - u*d.y - v*d.z + w*d.s - x*d.t - y*d.u - z*d.v,
-			s*d.x + t*d.w + u*d.z - v*d.y + w*d.t + x*d.s + y*d.v - z*d.u,
-			s*d.y - t*d.z + u*d.w + v*d.x + w*d.u - x*d.v + y*d.s + z*d.t,
-			s*d.z + t*d.y - u*d.x + v*d.w + w*d.v + x*d.u - y*d.t + z*d.s
-		};
-	}
-	template<class C>
-	auto operator+(DualQuaternion<C> const& r) -> DualQuaternion<std::common_type_t<S,C>> const {
-		return {s + r.s, t + r.t, u + r.u, v + r.v, w + r.w, x + r.x, y + r.y, z + r.z};
-	}
-	template<class C>
-	auto operator-(DualQuaternion<C> const& r) -> DualQuaternion<std::common_type_t<S,C>> const {
-		return {s - r.s, t - r.t, u - r.u, v - r.v, w - r.w, x - r.x, y - r.y, z - r.z};
-	}
+
+	/* If q is a rotation, does adding a rotation make sense?
+	 * Adding to the dual components might, if only non-duals could represent
+	 * translation on their own.
+	 * Even adding dual quaternions to one another is only intended for construction.
+	 */
+	template<class T>
+	DualQuaternion& operator+=(T const& t) { return *this = *this + t; }
+	template<class T>
+	DualQuaternion& operator-=(T const& t) { return *this = *this - t; }
+
+	template<class C, class SC = std::common_type_t<S,C>>
+	DualQuaternion<SC> operator+(DualQuaternion<C> const& r) const
+		{ return {s + r.s, t + r.t, u + r.u, v + r.v, w + r.w, x + r.x, y + r.y, z + r.z}; }
+
+	template<class C, class SC = std::common_type_t<S,C>>
+	DualQuaternion<SC> operator-(DualQuaternion<C> const& r) const
+		{ return {s - r.s, t - r.t, u - r.u, v - r.v, w - r.w, x - r.x, y - r.y, z - r.z}; }
+
 	/** (Right) scalar product. */
-	template<class C> DualQuaternion operator*(C const& c) const
+	template<class C, class SC = std::common_type_t<S,C>>
+	DualQuaternion<SC> operator*(C const& c) const
 		{ return {s*c, t*c, u*c, v*c, w*c, x*c, y*c, z*c}; }
 	/** (Left) scalar product. */
-	template<class C> friend DualQuaternion operator*(C const& c, DualQuaternion const& d)
+	template<class C, class SC = std::common_type_t<S,C>>
+	friend DualQuaternion<SC> operator*(C const& c, DualQuaternion const& d)
 		{ return {c*d.s, c*d.t, c*d.u, c*d.v, c*d.w, c*d.x, c*d.y, c*d.z}; }
+	template<class C, class SC = std::common_type_t<S,C>>
+	DualQuaternion<SC> operator/(Quaternion<C> const& c)
+		{ return *this * *c / c.lengthSquared(); }
+	template<class C, class SC = std::common_type_t<S,C>>
+	DualQuaternion<SC> operator/(C const& c)
+		{ return {s/c, t/c, u/c, v/c, w/c, x/c, y/c, z/c}; }
 
 	template<class T>
 	operator DualQuaternion<T>(void) {
 		return {T(s), T(t), T(u), T(v), T(w), T(x), T(y), T(z)};
 	}
-	template<class T>
+	template<class C, class SC = std::common_type_t<S,C>>
+	std::enable_if_t<!std::is_same<DualQuaternion<S>, C>::value, DualQuaternion<SC>>
+	operator*=(C const& c) { return *this = *this * c; }
+	template<class C>
+	DualQuaternion& operator*=(Quaternion<C> const& c) {
+		return *this = *this * c;
+	}
+	template<class C>
+	DualQuaternion& operator*=(DualQuaternion<C> const& c) {
+		return *this = *this * c;
+	}
+	/*template<class T>
 	DualQuaternion& operator*=(DualQuaternion<T> const& t) {
 		return *this = *this * DualQuaternion<S>{t.s, t.t, t.u, t.v, t.w, t.x, t.y, t.z};
-	}
-	template<class T>
-	DualQuaternion& operator*=(T const& t) { return *this = *this * t; }
+	}*/
 
 	template<class T>
-	DualQuaternion& operator+=(DualQuaternion<T> const& t) {
-		return *this = *this + DualQuaternion<S>{t.s, t.t, t.u, t.v, t.w, t.x, t.y, t.z};
-	}
+	DualQuaternion& operator+=(DualQuaternion<T> const& t)
+		{ return *this = *this + DualQuaternion<S>{t.s, t.t, t.u, t.v, t.w, t.x, t.y, t.z}; }
 	template<class T>
-	DualQuaternion& operator-=(DualQuaternion<T> const& t) {
-		return *this = *this - DualQuaternion<S>{t.s, t.t, t.u, t.v, t.w, t.x, t.y, t.z};
-	}
+	DualQuaternion& operator-=(DualQuaternion<T> const& t)
+		{ return *this = *this - DualQuaternion<S>{t.s, t.t, t.u, t.v, t.w, t.x, t.y, t.z}; }
 	template<class T>
-	DualQuaternion& operator/=(Quaternion<T> const& q) {
-		return *this = *this / Quaternion<S>{q.w, q.x, q.y, q.z};
-	}
+	DualQuaternion& operator/=(Quaternion<T> const& q)
+		{ return *this = *this / Quaternion<S>{q.w, q.x, q.y, q.z}; }
 
-	/* First define operator+(T), operator-(T) if you want these, e.g.
-	 *   template<class T> DualQuaternion operator+(Quaternion<T> const& q)
-	 * If q is a rotation, does adding a rotation make sense?
-	 * Adding to the dual components might, but quaternions can't represent translation on their
-	 * own, so this interpretation is a problem, too.
-	 * Even adding dual quaternions to one another is only intended for construction.
-	 */
-	/*
-	template<class T>
-	DualQuaternion& operator+=(T const& t) { return *this = *this + t; }
-	template<class T>
-	DualQuaternion& operator-=(T const& t) { return *this = *this - t; }
-	*/
-
+	template<class T, class ST = std::common_type_t<S,T>>
+	std::enable_if_t<std::is_arithmetic<T>::value, DualQuaternion<ST>>
+	operator^(T const& r) const {
+		return {}; // TODO
+		//return (DualQuaternion<ST>)*this;
+	}
+	template<class T, class V = typename T::value_type, class SV = std::common_type_t<S,V>>
+	DualQuaternion<SV> operator^(T const& r) const { return r * *this * *r; }
 
 	/** Equality operator; true if and only if all members are identical. */
 	bool operator==(DualQuaternion const& d) const
@@ -128,53 +138,95 @@ template<class S> struct DualQuaternion {
 	operator std::string(void) const;
 };
 
-/** Stream insertion operator; left generic to support ostringstream, etc. exactly. */
-template<class L, class S>
-L& operator<<(L&, DualQuaternion<S> const&);
+// TODO interpretation of dot product? Mind the dual*dual components?
+template<class S, class T>
+std::common_type_t<S,T> dot(DualQuaternion<S> const& l, DualQuaternion<T> const& r) {
+	return l.s*r.s + l.t*r.t + l.u*r.u + l.v*r.v
+			+ l.w*r.w + l.x*r.x + l.y*r.y + l.z*r.z;
+}
 
-template<class L, class R, class T>
-auto sclerp(DualQuaternion<L> const& lhs, DualQuaternion<R> const& rhs, T && t)
-		-> DualQuaternion<std::common_type_t<L,R,T>> {
-	Quaternion<L> lu = {lhs.s, lhs.t, lhs.u, lhs.v}, lv = {lhs.w, lhs.x, lhs.y, lhs.z};
-	Quaternion<R> ru = {rhs.s, rhs.t, rhs.u, rhs.v}, rv = {rhs.w, rhs.x, rhs.y, rhs.z};
-	auto ou = slerp(lu, ru, t), ov = lerp(lv, rv, t);
+template<class L, class R = L, class T = L, class LRT = std::common_type_t<L,R,T>>
+DualQuaternion<LRT> sclerp(DualQuaternion<L> const& lhs, DualQuaternion<R> const& rhs, T && t) {
+	auto ou = slerp<LRT>({lhs.s, lhs.t, lhs.u, lhs.v},
+			{rhs.s, rhs.t, rhs.u, rhs.v}, t),
+		ov = lerp<LRT>({lhs.w, lhs.x, lhs.y, lhs.z}, {rhs.w, rhs.x, rhs.y, rhs.z}, t);
+	/*auto ou = slerp(Quaternion<L>{lhs.s, lhs.t, lhs.u, lhs.v},
+			Quaternion<L>{lhs.w, lhs.x, lhs.y, lhs.z}, t),
+		ov = lerp(Quaternion<R>{rhs.s, rhs.t, rhs.u, rhs.v},
+			Quaternion<R>{rhs.w, rhs.x, rhs.y, rhs.z}, t);*/
 	return {ou.w, ou.x, ou.y, ou.z, ov.w, ov.x, ov.y, ov.z};
 }
-template<class L1, class R1, class L2, class R2, class S, class T>
+template<class L1, class R1 = L1, class L2 = L1, class R2 = R1, class S = L1, class T = S>
 DualQuaternion<std::common_type_t<L1,R1,L2,R2,S,T>>
 sclerp(DualQuaternion<L1> const& l1, DualQuaternion<R1> const& r1,
 		DualQuaternion<L2> const& l2, DualQuaternion<R2> const& r2,
-		S && s, T && t) {
-	return sclerp(sclerp(l1, r1, s), sclerp(l2, r2, s), t);
-}
+		S && s, T && t)
+	{ return sclerp(sclerp(l1, r1, s), sclerp(l2, r2, s), t); }
+
 
 template<class S>
-std::string to_string(DualQuaternion<S> const& d);
-template<class S>
-std::string to_string(DualQuaternion<S> const& d, unsigned prec);
+Quaternion<S>::operator DualQuaternion<S>(void) const
+	{ return {w, x, y, z}; }
+
+template<class S, class C, class SC = std::common_type_t<S,C>>
+DualQuaternion<SC> operator*(DualQuaternion<S> const& l, Quaternion<C> const& r) {
+	auto u = Quaternion<SC>{l.s, l.t, l.u, l.v} * r,
+		v = Quaternion<SC>{l.w, l.x, l.y, l.z} * r;
+	return {u.w, u.x, u.y, u.z, v.w, v.x, v.y, v.z};
+}
+
+template<class S, class C, class SC = std::common_type_t<S,C>>
+DualQuaternion<SC> operator*(Quaternion<S> const& l, DualQuaternion<C> const& r) {
+	return {
+		l.w*r.s - l.x*r.t - l.y*r.u - l.z*r.v,
+		l.w*r.t + l.x*r.s + l.y*r.v - l.z*r.u,
+		l.w*r.u + l.y*r.s - l.x*r.v + l.z*r.t,
+		l.w*r.v + l.z*r.s + l.x*r.u - l.y*r.t,
+		l.w*r.w - l.x*r.x - l.y*r.y - l.z*r.z,
+		l.w*r.x + l.x*r.w + l.y*r.z - l.z*r.y,
+		l.w*r.y - l.x*r.z + l.y*r.w + l.z*r.x,
+		l.w*r.z + l.x*r.y - l.y*r.x + l.z*r.w
+	};
+}
+template<class S, class C, class SC = std::common_type_t<S,C>>
+DualQuaternion<SC> operator*(DualQuaternion<S> const& l, DualQuaternion<C> const& r) {
+		return {
+			l.s*r.s - l.t*r.t - l.u*r.u - l.v*r.v,  l.s*r.t + l.t*r.s + l.u*r.v - l.v*r.u,
+			l.s*r.u + l.u*r.s - l.t*r.v + l.v*r.t,  l.s*r.v + l.v*r.s + l.t*r.u - l.u*r.t,
+			l.s*r.w - l.t*r.x - l.u*r.y - l.v*r.z + l.w*r.s - l.x*r.t - l.y*r.u - l.z*r.v,
+			l.s*r.x + l.t*r.w + l.u*r.z - l.v*r.y + l.w*r.t + l.x*r.s + l.y*r.v - l.z*r.u,
+			l.s*r.y - l.t*r.z + l.u*r.w + l.v*r.x + l.w*r.u - l.x*r.v + l.y*r.s + l.z*r.t,
+			l.s*r.z + l.t*r.y - l.u*r.x + l.v*r.w + l.w*r.v + l.x*r.u - l.y*r.t + l.z*r.s
+		};
+}
+template<class S, class C, class SC = std::common_type_t<S,C>>
+std::enable_if_t<!std::is_same<S,C>::value, DualQuaternion<SC>>
+operator+(DualQuaternion<S> const& lhs, DualQuaternion<C> const& rhs) {
+	DualQuaternion<SC> l = lhs, r = rhs;
+	return l + r;
+}
+
+/** Stream insertion operator; left generic to support ostringstream, etc. exactly. */
+template<class L, class S> L& operator<<(L&, DualQuaternion<S> const&);
+template<class S> std::string to_string(DualQuaternion<S> const& d);
+template<class S> std::string to_string(DualQuaternion<S> const& d, unsigned prec);
 
 // Gosh, it sure would be nice to have templates right about now.
 // Template user-defined literals require that the template argument is char...,
 // which would force parsing to play a role in literals.
-#define USERDEF_TEMPLATE(CLASS,PARAM,UD,S,T,U,V,W,X,Y,Z) \
-CLASS<PARAM> operator"" UD(long long unsigned d) { \
-	return {PARAM(S), PARAM(T), PARAM(U), PARAM(V), PARAM(W), PARAM(X), PARAM(Y), PARAM(Z)}; \
-} \
-CLASS<PARAM> operator"" UD(long double d) { \
-	return {PARAM(S), PARAM(T), PARAM(U), PARAM(V), PARAM(W), PARAM(X), PARAM(Y), PARAM(Z)}; \
-}
-#define USERDEF_TEMPLATES(CLASS, P1, UD1, P2, UD2, S, T, U, V, W, X, Y, Z) \
-	USERDEF_TEMPLATE(CLASS, P1, UD1, S, T, U, V, W, X, Y, Z) \
-	USERDEF_TEMPLATE(CLASS, P2, UD2, S, T, U, V, W, X, Y, Z)
+#define USERDEF_TEMPLATE(CLASS,PARAM,UD,W,X,Y,Z) \
+CLASS<PARAM> operator"" UD(long long unsigned d) \
+	{ return {0, 0, 0, 0, PARAM(W), PARAM(X), PARAM(Y), PARAM(Z)}; } \
+CLASS<PARAM> operator"" UD(long double d) \
+	{ return {0, 0, 0, 0, PARAM(W), PARAM(X), PARAM(Y), PARAM(Z)}; }
+#define USERDEF_TEMPLATES(CLASS, P1, UD1, P2, UD2, W, X, Y, Z) \
+	USERDEF_TEMPLATE(CLASS, P1, UD1, W, X, Y, Z) \
+	USERDEF_TEMPLATE(CLASS, P2, UD2, W, X, Y, Z)
 
-USERDEF_TEMPLATES(DualQuaternion, float, _e, double, _de, d,0,0,0, 0,0,0,0)
-USERDEF_TEMPLATES(DualQuaternion, float, _i, double, _di, 0,d,0,0, 0,0,0,0)
-USERDEF_TEMPLATES(DualQuaternion, float, _j, double, _dj, 0,0,d,0, 0,0,0,0)
-USERDEF_TEMPLATES(DualQuaternion, float, _k, double, _dk, 0,0,0,d, 0,0,0,0)
-USERDEF_TEMPLATES(DualQuaternion, float, _E, double, _dE, 0,0,0,0, d,0,0,0)
-USERDEF_TEMPLATES(DualQuaternion, float, _I, double, _dI, 0,0,0,0, 0,d,0,0)
-USERDEF_TEMPLATES(DualQuaternion, float, _J, double, _dJ, 0,0,0,0, 0,0,d,0)
-USERDEF_TEMPLATES(DualQuaternion, float, _K, double, _dK, 0,0,0,0, 0,0,0,d)
+USERDEF_TEMPLATES(DualQuaternion, float, _E, double, _dE, d,0,0,0)
+USERDEF_TEMPLATES(DualQuaternion, float, _I, double, _dI, 0,d,0,0)
+USERDEF_TEMPLATES(DualQuaternion, float, _J, double, _dJ, 0,0,d,0)
+USERDEF_TEMPLATES(DualQuaternion, float, _K, double, _dK, 0,0,0,d)
 
 #undef USERDEF_TEMPLATE
 #undef USERDEF_TEMPLATES
