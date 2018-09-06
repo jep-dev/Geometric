3fdf37ac421e9d12248dc707858ab66500d403bb
John Petersen <jepetersen.dev@gmail.com>
2018-09-06 16:31:47 -0400

    Figured out a much more efficient way of applying a projection matrix, but while the math works out perfectly, the ongoing bug with uniforms is preventing me from using it. I've gone quite insane.

--- share/vert.glsl
+++ share/vert.glsl
@@ -2,70 +2,54 @@
 #define M_PI 3.1415926535897932384626433832795
 
 struct Dual { vec4 u, v; };
-
-// Conjugate of a quaternion
-vec4 conj(vec4 q) { return vec4(q.x, -q.yzw); }
-// Dual quaternion conjugate (composed dual and quaternion conjugates)
-Dual conj(Dual d) { return Dual(conj(d.u), vec4(-d.v.x, d.v.yzw)); }
-
-// Squared Euclidean norm (omits the square root in q.length())
-// Accelerates inverses, combined magnitudes (see next), etc.
-float len2(vec4 q) { return q.w*q.w + q.x*q.x + q.y*q.y + q.z*q.z; }
-// Squared Euclidean norm (see previous)
-float len2(Dual d) { return len2(d.u) + len2(d.v); }
-
-// Quaternion product
-vec4 mul(vec4 l, vec4 r) {
-	return vec4(
-		l.x*r.x - l.y*r.y - l.z*r.z - l.w*r.w,
-		l.x*r.y + l.y*r.x + l.z*r.w - l.w*r.z,
-		l.x*r.z - l.y*r.w + l.z*r.x + l.w*r.y,
-		l.x*r.w + l.y*r.z - l.z*r.y + l.w*r.x
-	);
-}
-// Dual product
-Dual mul(Dual l, Dual r) { return Dual(mul(l.u, r.u), mul(l.v, r.u) + mul(l.u, r.v)); }
-// Quaternion product with dual
-Dual mul(vec4 l, Dual r) { return Dual(mul(l, r.u), mul(l, r.v)); }
-// Dual product with quaternion
-Dual mul(Dual l, vec4 r) { return Dual(mul(l.u, r), mul(l.v, r)); }
-
-// Quaternion division (pre-normalized denominator)
-vec4 divn(vec4 n, vec4 d) { return mul(n, conj(d)); }
-// Quaternion division (unrestricted denominator)
-vec4 div(vec4 n, vec4 d) { return divn(n, d) / len2(d); }
-// Dual division by quaternion (pre-normalized denominator)
-Dual divn(Dual l, vec4 r) { return Dual(divn(l.u, r), divn(l.v, r)); }
-// Dual division by quaternion (unrestricted denominator)
-Dual div(Dual l, vec4 r) { return Dual(div(l.u, r), div(l.v, r)); }
-
-// Quaternion 'sandwich' product
-vec4 sandwich(vec4 u, vec4 v) { return mul(mul(u, v), conj(u)); }
-// Dual 'sandwich' product
-Dual sandwich(Dual u, Dual v) { return mul(mul(u, v), conj(u)); }
-// Quaternion 'sandwich' product of a dual
-Dual sandwich(vec4 l, Dual r) { return mul(mul(l, r), conj(l)); }
-// Dual 'sandwich' product of a quaternion
-Dual sandwich(Dual l, vec4 r) { return mul(mul(l, r), conj(l)); }
-
-
-
-uniform mat4 projection;
+Dual sandwich(Dual u, Dual v);
 
 const vec4 u0 = vec4(1,0,0,0), v0 = vec4(0,0,0,0);
-uniform vec4 u = u0, v = v0;
-uniform Dual model = Dual(u0, v0);
+//uniform vec4 model[2] = {u0, v0};
+uniform vec4 model[2] = {vec4(0, 1, 0, 0), vec4(0, 0, 0, -4)};
+
+// projection[2] = {vec4(1, 1, -2.2, 0), vec4(0, 0, -1.2, -1)};
 
 in vec4 pos_in;
 out vec4 pos_out;
 
 void main(){
-	Dual x = Dual(u, v);
-	x = Dual(u0, vec4(0, pos_in.xyz));
-	//Dual mxm = sandwich(model, x);
-	Dual mxm = sandwich(Dual(u, v), x);
-	//Dual mxm = sandwich(Dual(u0, v0), x);
+//	float right = 10, left = -right,
+//		top = 10, bottom = -top,
+//		near = 1, far = 10;
+//	mat4 projection = mat4(
+//		2*near/right, 0, (right + left) / (right - left), 0,
+//		0, 2*near/top, (top + bottom) / (top - bottom), 0,
+//		0, 0, 2*near*far/(near-far), (near+far)/(near-far),
+//		0, 0, -1, 0);
 
-	gl_Position = projection * vec4(mxm.v.yzw, 1);
+	mat4 projection = mat4(
+		0.5, 0.0, 0.0, 0.0,
+		0.0, 0.5, 0.0, 0.0,
+		0.0, 0.0,-1.2,-2.2,
+		0.0, 0.0,-1.0, 0.0
+	);
+	Dual d = sandwich(Dual(model[0], model[1]), Dual(u0, vec4(0, pos_in.xyz)));
+	vec4 newpos = vec4(d.v.yzw, pos_in.w);
+	//vec4 newpos = vec4(d.v.yzw, 1);
+	gl_Position = projection * newpos;
+	//gl_Position = proj0 * newpos + proj1 * newpos
+		//+ proj2 * newpos + proj3 * newpos;
+
+
+
+	//gl_Position = proj * pos_in;
+	//  x = d.v.w; y = d.v.x; z = d.v.y; w = pos_in.w
+	//Dual d = sandwich(Dual(model[0], model[1]), Dual(u0, vec4(0, pos_in.xyz)));
+	//gl_Position = projection[0] * vec4(d.v.wx, pos_in.w, 0) + d.v.y * projection[1];
+	//gl_Position = projection[0] * vec4(pos_in.xyw, 0) + pos_in.z * projection[1];
+
+
+//	float w = pos_in.w, x = d.v.y, y = d.v.z, z = d.v.w;
+	// Aiming for multiply-add boost, and less operations than matrix
+//	gl_Position = 2 * near * vec4( x / right, y / top, w * far / (near - far), 0)
+//		+ z * vec4((right + left) / (right - left), (top + bottom) / (top - bottom),
+//				(far + near) / (near - far), -1);
+
 	pos_out = gl_Position;
 }
