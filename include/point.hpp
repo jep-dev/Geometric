288f469518ab6812c17bde4927c728825c7ab580
John Petersen <jepetersen.dev@gmail.com>
2018-09-06 16:30:08 -0400

    Added point type; points are perfectly representable in dual quaternions, but operations on points with implied dual representation are much faster (5/8 values are constants, 4 of which are zero and can be neglected)

---
+++ include/point.hpp
@@ -0,0 +1,60 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+#include "math.hpp"
+
+template<class S>
+struct Point {
+	S x = 0, y = 0, z = 0;
+	template<class T, class ST = std::common_type_t<S,T>>
+	Point<ST> operator+(Point<T> const& p) const { return {x+p.x, y+p.y, z+p.z}; }
+	template<class T>
+	Point<S>& operator+=(Point<T> const& p)
+		{ return x += S(p.x), y += S(p.y), z += S(p.z), *this; }
+
+	template<class T, class ST = std::common_type_t<S,T>>
+	Point<ST> operator-(Point<T> const& p) const { return {x-p.x, y-p.y, z-p.z}; }
+	template<class T>
+	Point<S>& operator-=(Point<T> const& p)
+		{ return x -= S(p.x), y -= S(p.y), z -= S(p.z), *this; }
+
+	template<class T, class ST = std::common_type_t<S,T>>
+	Point<ST> operator^(Quaternion<T> const& t) const {
+		ST p = -t.x*x - t.y*y - t.z*z, q = t.w*x + t.y*z - t.z*y,
+			r = t.w*y - t.x*z + t.z*x, s = t.w*z + t.x*y - t.y*x;
+		return {
+			-p*t.x + q*t.w - r*t.z + s*t.y,
+			-p*t.y + q*t.z + r*t.w - s*t.x,
+			-p*t.z - q*t.y + r*t.x + s*t.w
+		};
+	}
+	template<class T, class ST = std::common_type_t<S,T>>
+	Point<ST> operator^(DualQuaternion<T> const& t) const {
+		ST p = -t.t*x - t.u*y - t.v*z, q = t.s*x + t.u*z - t.v*y,
+			r = t.s*y - t.t*z + t.v*x, s = t.s*z + t.t*y - t.u*x;
+		return {
+			-p*t.t + q*t.s - r*t.v + s*t.u
+				+ 2*(-t.w*x + t.x*t.s),
+			-p*t.u + q*t.v + r*t.s - s*t.t
+				+ 2*(-t.w*y + t.y*t.s),
+			-p*t.v - q*t.u + r*t.t + s*t.s
+				+ 2*(-t.w*z + t.z*t.s)
+		};
+	}
+
+	friend struct DualQuaternion<S>;
+	operator DualQuaternion<S>(void) const { return {1, 0, 0, 0, 0, x, y, z}; }
+	template<class T>
+	friend T& operator<<(T &t, Point const& p) { return t << (DualQuaternion<S>) p, t; }
+	operator std::string(void) const { return std::string((DualQuaternion<S>) *this); }
+};
+template<class S>
+std::string to_string(Point<S> const& p)
+	{ return "<" + to_string(p.x) + ", " + to_string(p.y) + ", " + to_string(p.z) + ">"; }
+template<class S>
+std::string to_string(Point<S> const& p, unsigned prec) {
+	return "<" + to_string(p.x, prec) + ", " + to_string(p.y, prec)
+			+ ", " + to_string(p.z, prec) + ">";
+}
+
+#endif
