e9f8fd14f07ed36a320a3b3032393937a53d061b
John Petersen <jepetersen.dev@gmail.com>
2018-09-08 16:07:12 -0400

    Fixed long-standing issue with uniforms!

--- share/vert.glsl
+++ share/vert.glsl
@@ -2,54 +2,29 @@
 #define M_PI 3.1415926535897932384626433832795
 
 struct Dual { vec4 u, v; };
+
+// Dual 'sandwich' product
 Dual sandwich(Dual u, Dual v);
 
-const vec4 u0 = vec4(1,0,0,0), v0 = vec4(0,0,0,0);
-//uniform vec4 model[2] = {u0, v0};
-uniform vec4 model[2] = {vec4(0, 1, 0, 0), vec4(0, 0, 0, -4)};
+//uniform mat4 projection;
+uniform float a=1,
+	r = 2, l = -2,
+	t = 2, b = -2,
+	n = 1, f = 10;
 
-// projection[2] = {vec4(1, 1, -2.2, 0), vec4(0, 0, -1.2, -1)};
+const vec4 u0 = vec4(1,0,0,0), v0 = vec4(0,0,0,0);
+uniform Dual model = Dual(u0, v0);
 
 in vec4 pos_in;
 out vec4 pos_out;
 
 void main(){
-//	float right = 10, left = -right,
-//		top = 10, bottom = -top,
-//		near = 1, far = 10;
-//	mat4 projection = mat4(
-//		2*near/right, 0, (right + left) / (right - left), 0,
-//		0, 2*near/top, (top + bottom) / (top - bottom), 0,
-//		0, 0, 2*near*far/(near-far), (near+far)/(near-far),
-//		0, 0, -1, 0);
-
-	mat4 projection = mat4(
-		0.5, 0.0, 0.0, 0.0,
-		0.0, 0.5, 0.0, 0.0,
-		0.0, 0.0,-1.2,-2.2,
-		0.0, 0.0,-1.0, 0.0
-	);
-	Dual d = sandwich(Dual(model[0], model[1]), Dual(u0, vec4(0, pos_in.xyz)));
-	vec4 newpos = vec4(d.v.yzw, pos_in.w);
-	//vec4 newpos = vec4(d.v.yzw, 1);
-	gl_Position = projection * newpos;
-	//gl_Position = proj0 * newpos + proj1 * newpos
-		//+ proj2 * newpos + proj3 * newpos;
-
-
-
-	//gl_Position = proj * pos_in;
-	//  x = d.v.w; y = d.v.x; z = d.v.y; w = pos_in.w
-	//Dual d = sandwich(Dual(model[0], model[1]), Dual(u0, vec4(0, pos_in.xyz)));
-	//gl_Position = projection[0] * vec4(d.v.wx, pos_in.w, 0) + d.v.y * projection[1];
-	//gl_Position = projection[0] * vec4(pos_in.xyw, 0) + pos_in.z * projection[1];
-
-
-//	float w = pos_in.w, x = d.v.y, y = d.v.z, z = d.v.w;
-	// Aiming for multiply-add boost, and less operations than matrix
-//	gl_Position = 2 * near * vec4( x / right, y / top, w * far / (near - far), 0)
-//		+ z * vec4((right + left) / (right - left), (top + bottom) / (top - bottom),
-//				(far + near) / (near - far), -1);
-
+	Dual x0 = Dual(u0, vec4(0, pos_in.xyz)),
+		s = sandwich(model, x0);
+	float aspect = sqrt(a);
+	//gl_Position = 2*n*vec4(1/r, 1/t, f/(n-f), 0) * vec4(s.v.yz, pos_in.w, 1)
+	//	+ s.v.w * vec4((r+l)/(r-l), (t+b)/(t-b), (n+f)/(n-f), -1);
+	gl_Position = 2*n*vec4(aspect/r, 1/(aspect*t), f/(n-f), 0) * vec4(s.v.yz, pos_in.w, 1)
+		+ s.v.w * vec4(aspect*(r+l)/(r-l), (t+b)/(aspect*(t-b)), (n+f)/(n-f), -1);
 	pos_out = gl_Position;
 }
