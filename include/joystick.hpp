d424208f44449d46e90da60e4dd0b1f8b9e1532c
John Petersen <jepetersen.dev@gmail.com>
2018-09-20 18:05:18 -0400

    Added 'Joystick' type with an id, SDL joystick, axes, buttons, and hat (direction pad); added basic open, close, and conversion operators

--- include/joystick.hpp
+++ include/joystick.hpp
@@ -18,4 +18,39 @@ std::pair<float, float> deadzone(int x, int y, int max_value, float dead) {
 	return deadzone(fx, fy, dead);
 }
 
+struct Joystick {
+	template<class S = Uint8, class T = S>
+	using History = std::pair<S,T>;
+	SDL_JoystickID id;
+	SDL_Joystick *joystick = 0;
+	std::map<unsigned, float> axes;
+	std::map<unsigned, History<>> buttons;
+	History<> hat;
+
+	operator SDL_Joystick*(void) const { return joystick; }
+	operator bool(void) const //{ return joystick; }
+		{ return SDL_JoystickGetAttached(joystick); }
+	void open(void) {
+		open(id);
+	}
+	bool open(unsigned i) {
+		close();
+		id = i;
+		joystick = SDL_JoystickOpen(id);
+		return joystick;
+		//return *this;
+	}
+	void close(void) {
+		//if(SDL_JoystickGetAttached(joystick))
+			SDL_JoystickClose(joystick);
+		joystick = 0;
+	}
+	Joystick(unsigned i) : id(id) {
+		open();
+	}
+	virtual ~Joystick(void) {
+		close();
+	}
+};
+
 #endif
