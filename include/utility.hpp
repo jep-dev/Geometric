d0504b5c68ea8ac8f4faa7b7cfce6fa11fe0a5c8
John Petersen <jepetersen.dev@gmail.com>
2018-09-30 16:47:20 -0400

    Added 'ValueType[_t]', which recurses on value_types to find the innermost type, and Add_ValueType[_t], which finds the result of addition between value_types (e.g. A<S> + B<C<T>> -> S+T)

--- include/utility.hpp
+++ include/utility.hpp
@@ -29,6 +29,40 @@ struct Seq{}; //= Tag<std::integral_constant<unsigned, I>...>;
 
 template<class... T> struct Void { using value_type = void; };
 
+template<class T, class SFINAE = void>
+struct ValueType {
+	typedef T type;
+};
+
+template<class... T>
+using ValueType_t = typename ValueType<T...>::type;
+
+template<class T>
+struct ValueType<T, Detail::Void_t<typename T::value_type>> {
+	typedef ValueType_t<typename T::value_type> type;
+};
+
+template<class S, class... T>
+auto get_value_type(Detail::Tag<S> = {}) -> ValueType_t<S, T...> { return {}; }
+
+template<class... T>
+using Common_ValueType_t = std::common_type_t<ValueType_t<T>...>;
+
+template<class S, class T, class... U>
+struct Add_ValueType;
+
+template<class S, class T, class... U>
+struct Add_ValueType {
+	using value_type = decltype(std::declval<ValueType_t<S>>()
+		+ std::declval<ValueType_t<T>>());
+};
+
+template<class... T> using Add_ValueType_t = ValueType_t<Add_ValueType<T...>>;
+
+template<class R, class S, class T, class... U>
+struct Add_ValueType<R, S, T, U...> {
+	using value_type = Add_ValueType_t<Add_ValueType_t<Add_ValueType_t<R, S>, T>, U...>;
+};
 }
 
 #endif
