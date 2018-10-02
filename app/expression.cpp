ebed4b28a6b79d689a2100d2a0fab512766fc51c
John Petersen <jepetersen.dev@gmail.com>
2018-10-02 03:16:09 -0400

    Added 'print_derivatives' to print a derivative of an expression with respect to each given dimension; added some symbols and calls to print_derivatives; currently, use of products causes a null pointer exception (commented out.)

--- app/expression.cpp
+++ app/expression.cpp
@@ -9,25 +9,33 @@
 #include "streams.tpp"
 #include "utility.hpp"
 
-
+template<class S, class... T>
+std::ostream& print_derivatives(std::ostream &os, std::shared_ptr<S> const& s, T &&... t) {
+	using namespace std;
+	for(auto it : {t...})
+		os << "d/d" << it << " " << *s << " = " << *(s -> derive(it)) << endl;
+	return os;
+}
 
 int main(int argc, const char *argv[]) {
 	using namespace std;
 	using namespace Detail;
+	using namespace Expressions;
+
 	using T = float;
 	using DQ = DualQuaternion<T>;
-	using namespace Expressions;
-	using Sym = Symbol<DQ>;
-	using SymPtr = std::shared_ptr<Sym>;
-	SymPtr one(new Scalar<DQ>(1_e)), u(new Variable<DQ>('x')),
-		sum(new Sum<DQ>(std::move(one), std::move(u)));
-	std::map<char, DQ> vars = {
-		{'u', DQ{1}}
-	};
-	cout << *sum << endl;
 
-	/*Cosine<T> c;
-	Sine<T> s;
-	cout << Pretty<decltype(c)>() << " -> " << Pretty<decltype(c.derive())>() << endl;
-	cout << Pretty<decltype(s)>() << " -> " << Pretty<decltype(s.derive())>() << endl;*/
+	/*std::map<char, DQ> vars = {
+		{'u', DQ{1}}
+	};*/
+
+	auto u = make_variable<DQ>('U');
+	auto uu = make_product<DQ>(u, u);
+	auto v = make_scalar<DQ>(DQ(1_e));
+	auto uv = make_sum<DQ>(v, v);
+
+	print_derivatives(cout, u, 'U');
+	//print_derivatives(cout, uu, 'U');
+	print_derivatives(cout, v, 'U');
+	print_derivatives(cout, uv, 'U');
 }
