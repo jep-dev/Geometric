12696d3d8ee3bfe2b7d85d7d6a43098b5b2b819b
John Petersen <jepetersen.dev@gmail.com>
2018-10-06 19:23:46 -0400

    Mainly removed unused code

--- share/frag.glsl
+++ share/frag.glsl
@@ -1,7 +1,7 @@
 #version 330
 #define M_PI 3.1415926535897932384626433832795
 
-in vec4 pos_out;
+in vec4 pos;
 
 out vec4 color;
 
@@ -22,27 +22,15 @@ float atan2(float y, float x) {
 }
 
 void main(){
-	float x = pos_out.x, y = pos_out.y, z = pos_out.z,
-			ax = abs(x), ay = abs(y), az = abs(z),
-			rxz = sqrt(ax*ax+az*az),
-			t = atan2(rxz, ay)*16,
-			//t = (atan(rxz, ay)*4)*8,
-			t2 = t + M_PI*2/3, t3 = t + M_PI*4/3;
-	color = vec4(nsin(t), nsin(t2), nsin(t3), exp(-z*z));
-	float hue = M_PI*cos(x*M_PI*2)*cos(y*M_PI*2)*cos(z*M_PI*2)*ncos(x*x+y*y+z*z);
-	color = vec4(ncos(hue), ncos(hue+M_PI*2/3), ncos(hue+M_PI*4/3), 1);
-
-
-	//color = vec4(ax/axyz, ay/axyz, az/axyz, 1);
-	//color = vec4(ax*pcx, ay*pcy, az*pcz, 1);
-	//color = vec4(abs(pos_out.x)/4, abs(pos_out.y)/4, abs(pos_out.z)/4, 1);
-	//color = vec4(renorm(cos(pos_out.x*M_PI)),
-		//renorm(cos(pos_out.y*M_PI)),
-		//renorm(cos(pos_out.z*M_PI)), 1);
-	//color = pos_out * pos_out / length(pos_out.xyz);
-
-	//vec3 normed = vec3(w.y*w.y, w.z*w.z, w.w*w.w);
-	//vec3 normed = normalize(vec3(w.yzw))*0.5 + vec3(0.5, 0.5, 0.5);
-	//color = vec4(normed, 1);
-	//color = vec4(w.yzw, 1);
+	float x = pos.x*2*M_PI, y = pos.y*2*M_PI, z = pos.z*2*M_PI,
+		cx = ncos(x*2*M_PI), cy = ncos(y*2*M_PI), cz = ncos(z*2*M_PI),
+		x2 = x*x, y2 = y*y, z2 = z*z,
+		xy2 = x2+y2, xz2 = x2+z2, yz2 = y2+z2, xyz2 = xy2+z2,
+		xy = sqrt(xy2), xz = sqrt(xz2), yz = sqrt(yz2),
+			cxy = ncos(xy*M_PI*2), cyz = ncos(yz*M_PI*2), cxz = ncos(xz*M_PI*2),
+			sxy = nsin(xy*M_PI*2), syz = nsin(yz*M_PI*2), sxz = nsin(xz*M_PI*2),
+		xyz = sqrt(xyz2), cxyz = ncos(xyz*M_PI*2),
+		h = M_PI*(cos(x)+cos(y)+cos(z)),
+		b = cos(2*atan2(cx,(cx*cx+cy*cy+cz*cz)));
+	color = vec4(ncos(h)*b, ncos(h+M_PI*2/3)*b, ncos(h+M_PI*4/3)*b, 1);
 }
