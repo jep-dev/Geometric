dd9c5dac2a463d65dce5599834f91033239c0fa2
John Petersen <jepetersen.dev@gmail.com>
2018-11-13 15:44:57 -0500

    Added prepend/append values/sequences; added some comments to functions/types; added a stream insertion operator for Seq (prints {I0, ...}); added SumSeq and revised ProductSeq (these are analogous to sigma/sum and pi/product.)

--- include/sequence.hpp
+++ include/sequence.hpp
@@ -29,29 +29,73 @@ template<long... I> using SeqL = Seq<long, I...>;
  * or undefined in general, which might suggest Seq*<> should be undefined after all.
  */
 
+/** Prepends a value to a sequence. */
+template<class T, T I0, T... IN>
+constexpr auto prepend(Detail::Seq<T, IN...> = {})
+	-> Detail::Seq<T, I0, IN...> { return {}; }
+/** Appends a value to a sequence (same as prepend in reverse order) */
+template<class T, T I0, T... IN>
+constexpr auto append(Detail::Seq<T, IN...> = {})
+	-> Detail::Seq<T, IN..., I0> { return {}; }
+/** Prepends a sequence to another. */
+template<class T, T... I0, T... I1>
+constexpr auto prepend(Detail::Seq<T, I0...> = {}, Detail::Seq<T, I1...> = {})
+	-> Detail::Seq<T, I0..., I1...> { return {}; }
+/** Appends a sequence to another. */
+template<class T, T... I0, T... I1>
+constexpr auto append(Detail::Seq<T, I0...> = {}, Detail::Seq<T, I1...> = {})
+	-> Detail::Seq<T, I1..., I0...> { return {}; }
+
+template<class T, T... IN>
+constexpr std::size_t size(Detail::Seq<T, IN...> = {}) { return sizeof...(IN); }
+/** Pairwise sum of sequences */
 template<class TI, unsigned... I, class TJ, unsigned... J,
 		class T = SumType_t<TI, TJ>>
 constexpr auto operator+(Seq<TI, I...>, Seq<TJ, J...>) -> Seq<T, I+J...> { return {}; }
 
+/** Pairwise difference of sequences */
 template<class TI, unsigned... I, class TJ, unsigned... J,
-		class T = SumType_t<TI, TJ>>
+		class T = DifferenceType_t<TI, TJ>>
 constexpr auto operator-(Seq<TI, I...>, Seq<TJ, J...>) -> Seq<T, I-J...> { return {}; }
 
+/** Pairwise product of sequences */
 template<class TI, unsigned... I, class TJ, unsigned... J,
 		class T = ProductType_t<TI, TJ>>
 constexpr auto operator*(Seq<TI, I...>, Seq<TJ, J...>) -> Seq<T, I*J...> { return {}; }
 
+/** Pairwise division of sequences */
 template<class TI, unsigned... I, class TJ, unsigned... J,
 		class T = QuotientType_t<TI, TJ>>
 constexpr auto operator/(Seq<TI, I...>, Seq<TJ, J...>) -> Seq<T, I/J...> { return {}; }
 
-/** Defines value as the product of all of {IN...}.
+template<class OS, class S, S... I>
+OS& operator<<(OS &os, Detail::Seq<S, I...> const&) {
+	bool first = true;
+	os << '{';
+	for(auto i : {I...}) {
+		if(!first) os << ", ";
+		first = false;
+		os << i;
+	}
+	os << '}';
+	return os;
+}
+
+/** Defines value as the sum of all {I0, IN...}.
+ * Not to be confused with pairwise sum, etc. */
+template<class T, T I = 0, T... IN>
+struct SumSeq: std::integral_constant<T, I> {};
+template<class T, T I0, T I1, T... IN>
+struct SumSeq<T, I0, I1, IN...>: SumSeq<T, I0+I1, IN...> {};
+
+
+/** Defines value as the product of all of {I0, IN...}.
  * Not to be confused with inner product, etc. */
 //template<class T, T... IN> struct ProductSeq;
-template<class T, T I, T... IN> struct ProductSeq:
-	std::integral_constant<T, I> {};
-template<class T, T I, T I0, T... IN> struct ProductSeq<T, I, I0, IN...>:
-	std::integral_constant<T, ProductSeq<T, I*I0, IN...>::value> {};
+template<class T, T I0 = 1, T... IN>
+struct ProductSeq: std::integral_constant<T, I0> {};
+template<class T, T I0, T I1, T... IN>
+struct ProductSeq<T, I0, I1, IN...>: ProductSeq<T, I0*I1, IN...> {};
 
 }
 
