3c9a4d5e0893f79f3a97af131267e3580259db7e
John Petersen <jepetersen.dev@gmail.com>
2018-11-13 15:34:50 -0500

    Replaced 'size' with 'whichSize' (returns a string describing the chosen candidate) and wrote 'size' that actually uses the appropriate method/deduction; added comments to separate sections more clearly; added 'Printer' to use these methods to print the size and chosen candidate of a type; added STL types to tests to validate chosen candidates; added arrays to tests to show how candidates are chosen based on qualifiers, and how to manipulate those qualifiers properly.

--- app/size.cpp
+++ app/size.cpp
@@ -1,45 +1,27 @@
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 #include <tuple>
 #include <vector>
 
 #include "pretty.tpp"
 #include "utility.hpp"
+#include "adaptor.hpp"
 
-struct False : std::false_type { };
-struct True : std::true_type { };
+using False = std::false_type;
+using True = std::true_type;
 
-template<class S, class = void>
-struct HasLength : False { typedef False type; };
-template<class S, class = void>
-struct HasSize : False { typedef False type; };
-template<class S, class = void>
-struct HasTellp : False { typedef False type; };
-template<class S, class = void>
-struct HasSeekp : False { typedef False type; };
-template<class S, class = void>
-struct HasTellg : False { typedef False type; };
-template<class S, class = void>
-struct HasSeekg : False { typedef False type; };
+template<class S, class = void> struct HasSize : False { };
+template<class S, class = void> struct HasLength : False { };
+template<class S, class = void> struct HasDimension
+	: std::is_array<std::remove_const_t<std::remove_reference_t<S>>> { };
+	//: std::is_array<Detail::RemoveCVRef_t<S>> { };
+template<class S, class = void, class = void> struct HasTellpSeekp : False { };
+template<class S, class = void, class = void> struct HasTellgSeekg : False { };
 
-template<class S, class = void, class = void>
-struct HasTellpSeekp : False { typedef False type; };
-template<class S, class = void, class = void>
-struct HasTellgSeekg : False { typedef False type; };
-
-template<class S>
-struct HasLength<S, Detail::Void_t<decltype(&S::length)>>
-	: True { typedef decltype(&S::length) type; };
-template<class S>
-struct HasSize<S, Detail::Void_t<decltype(&S::size)>>
-	: True { typedef decltype(&S::size) type; };
-template<class S>
-struct HasTellg<S, Detail::Void_t<decltype(&S::tellg)>>
-	: True { typedef decltype(&S::tellg) type; };
-template<class S>
-struct HasTellp<S, Detail::Void_t<decltype(&S::tellp)>>
-	: True { typedef decltype(&S::tellp) type; };
+template<class S> struct HasSize<S, Detail::Void_t<decltype(&S::size)>> : True { };
+template<class S> struct HasLength<S, Detail::Void_t<decltype(&S::length)>> : True { };
 
 template<class S>
 struct HasTellpSeekp<S, Detail::Void_t<decltype(&S::tellp)>,
@@ -48,82 +30,297 @@ template<class S>
 struct HasTellgSeekg<S, Detail::Void_t<decltype(&S::tellg)>,
 		Detail::Void_t<decltype(&S::seekg)>> : True {};
 
+/////////////////////////////////// Arrays ///////////////////////////////////
+template<class T, unsigned N = 0> const char *whichSize(T const (&) [N]) { return "deduced const&"; }
+template<class T, unsigned N = 0> const char *whichSize(T (&) [N]) { return "deduced &"; }
+template<class T, unsigned N = 0> const char *whichSize(T (&&) [N]) { return "deduced"; }
+// TODO Why are these specs necessary? When N = 0, fallbacks are used otherwise.
+template<class T> const char* whichSize(T const (&) [0]) { return "deduced const&"; }
+template<class T> const char* whichSize(T (&&) [0]) { return "deduced &&"; }
+template<class T> const char* whichSize(T (&) [0]) { return "deduced &"; }
 
-template<class S, class T> using HasLength_t = typename HasLength<S, T>::type;
-template<class S, class T> using HasSize_t = typename HasSize<S, T>::type;
-template<class S, class T> using HasTellg_t  = typename HasTellg<S, T>::type;
-template<class S, class T> using HasTellp_t = typename HasTellp<S, T>::type;
+/* TODO Fallback candidates are preferred to these for some reason
+	template<class T> std::enable_if_t<HasDimension<T>::value, const char*>
+		whichSize(T const& t) { return "deduced"; }
+*/ // etc.
 
+template<class T, unsigned N = 0> std::size_t size(T const (&) [N]) { return N; }
+template<class T, unsigned N = 0> std::size_t size(T (&) [N]) { return N; }
+template<class T, unsigned N = 0> std::size_t size(T (&&) [N]) { return N; }
+template<class T> std::size_t size(T const (&) [0]) { return 0; }
+template<class T> std::size_t size(T (&&) [0]) { return 0; }
+template<class T> std::size_t size(T (&) [0]) { return 0; }
+
+/////////////////////////////////// Length ///////////////////////////////////
 /** Proof of concept for HasLength */
+template<class T> std::enable_if_t<HasLength<Detail::RemoveCVRef_t<T>>::value, const char*>
+	whichSize(T && t) { return "length"; }
 template<class T> std::enable_if_t<HasLength<T>::value, const char*>
-	size(T && t) { return t.length(), ".length()"; }
+	whichSize(T & t) { return "length"; }
+template<class T> std::enable_if_t<HasLength<Detail::RemoveCVRef_t<T>>::value, std::size_t>
+	size(T && t) { return t.length(); }
+template<class T> std::enable_if_t<HasLength<T>::value, std::size_t>
+	size(T & t) { return t.length(); }
+
+///////////////////////////////////// Size ///////////////////////////////////
 /** Proof of concept for HasSize; excludes types with length to remove ambiguity */
 template<class T> std::enable_if_t<HasSize<T>::value && !HasLength<T>::value, const char*>
-	size(T && t) { return t.size(), ".size()"; }
+	whichSize(T && t) { return "size"; }
+template<class T> std::enable_if_t<HasSize<T>::value && !HasLength<T>::value, std::size_t>
+	size(T && t) { return t.size(); }
 
 /* Removed due to ambiguity with HasTellpSeekp/HasTellgSeekg
 	// Proof of concept for HasTellp
 	template<class T> std::enable_if_t<HasTellp<T>::value, const char*>
-		size(T && t) { return t.tellp(), ".tellp()"; }
+		whichSize(T && t) { return t.tellp(), "tellp"; }
 	// Proof of concept for HasTellg
 	template<class T> std::enable_if_t<HasTellg<T>::value, const char*>
-		size(T && t) { return t.tellg(), ".tellg()"; }
+		whichSize(T && t) { return t.tellg(), "tellg"; }
 */
+
+///////////////////////////////////// Seek* //////////////////////////////////
 template<class T> std::enable_if_t<HasTellpSeekp<T>::value, const char*>
-	size(T && t) { return t.tellp(), t.seekp(0, std::ios::end), ".tellp()/.seekp()"; }
+	whichSize(T & t) { return t.tellp(), t.seekp(0, std::ios::end), "seekp"; }
 template<class T> std::enable_if_t<HasTellgSeekg<T>::value, const char*>
-	size(T && t) { return t.tellg(), t.seekg(0, std::ios::end), ".tellg()/.seekg()"; }
+	whichSize(T & t) { return t.tellg(), t.seekg(0, std::ios::end), "seekg"; }
+template<class T> std::enable_if_t<HasTellpSeekp<T>::value, const char*>
+	whichSize(T && t) { return t.tellp(), t.seekp(0, std::ios::end), "seekp"; }
+template<class T> std::enable_if_t<HasTellgSeekg<T>::value, const char*>
+	whichSize(T && t) { return t.tellg(), t.seekg(0, std::ios::end), "seekg"; }
 
-/** Fallback in case none are implemented; comment out to require one of the others */
-template<class T> std::enable_if_t<!HasTellp<T>::value && !HasTellg<T>::value
-		&& !HasLength<T>::value && !HasSize<T>::value, const char*>
-	size(T& t) { return "n/a (1)"; }
 template<class T>
-const char* size(T const& t) { return "n/a (2)"; }
+std::enable_if_t<HasTellpSeekp<T>::value, std::size_t> size(T & t) {
+	auto pos = t.tellp(), out = (t.seekp(0, std::ios::end), t.tellp());
+	return t.seekp(pos), out;
+}
+template<class T>
+std::enable_if_t<HasTellpSeekp<T>::value, std::size_t> size(T && t) {
+	auto pos = t.tellp(), out = (t.seekp(0, std::ios::end), t.tellp());
+	return t.seekp(pos), out;
+}
+template<class T>
+std::enable_if_t<HasTellgSeekg<T>::value, std::size_t> size(T & t) {
+	auto pos = t.tellg(), out = (t.seekg(0, std::ios::end), t.tellg());
+	return t.seekg(pos), out;
+}
+template<class T>
+std::enable_if_t<HasTellgSeekg<T>::value, std::size_t> size(T && t) {
+	auto pos = t.tellg(), out = (t.seekg(0, std::ios::end), t.tellg());
+	return t.seekg(pos), out;
+}
 
-struct A { constexpr std::size_t length(void) const { return __LINE__; } };
+/////////////////////////////////// Fallbacks ////////////////////////////////
+/** Fallback in case none are implemented; comment out to require one of the others */
+template<class T> std::enable_if_t<!std::is_array<T>::value
+		&& !HasTellpSeekp<T>::value && !HasTellgSeekg<T>::value
+		&& !HasSize<T>::value && !HasLength<T>::value, const char*>
+	whichSize(T & t) { return "fallback"; }
+template<class T> std::enable_if_t<!std::is_array<T>::value
+		&& !HasTellpSeekp<T>::value && !HasTellgSeekg<T>::value
+		&& !HasSize<T>::value && !HasLength<T>::value, const char*>
+	whichSize(T const& t) { return "fallback"; }
+template<class T> std::enable_if_t<!std::is_array<T>::value
+		&& !HasTellpSeekp<T>::value && !HasTellgSeekg<T>::value
+		&& !HasSize<T>::value && !HasLength<T>::value, const char*>
+	whichSize(T && t) { return "fallback"; }
+
+/*template<class T> std::enable_if_t<!std::is_array<T>::value
+		&& !HasTellpSeekp<T>::value && !HasTellgSeekg<T>::value
+		&& !HasSize<T>::value && !HasLength<T>::value, std::size_t>
+	size(T & t) { return 0; }
+template<class T> std::enable_if_t<!std::is_array<T>::value
+		&& !HasTellpSeekp<T>::value && !HasTellgSeekg<T>::value
+		&& !HasSize<T>::value && !HasLength<T>::value, std::size_t>
+	size(T const& t) { return 0; }*/
+template<class T> std::enable_if_t<!std::is_array<T>::value
+		&& !HasTellpSeekp<T>::value && !HasTellgSeekg<T>::value
+		&& !HasSize<T>::value && !HasLength<T>::value, std::size_t>
+	size(T && t) { return 0; }
+
+
+
+struct A { constexpr std::size_t length(void) const { return 0; } };
 struct B {
-	constexpr std::size_t tellp(void) const { return __LINE__; }
-	constexpr std::size_t seekp(int, const std::ios::seek_dir &) const { return __LINE__; }
+	constexpr std::size_t tellp(void) const { return 0; }
+	constexpr std::size_t seekp(int, const std::ios::seek_dir & = std::ios::end) const { return 0; }
 };
 struct C {
-	constexpr std::size_t tellg(void) const { return __LINE__; }
-	constexpr std::size_t seekg(int, const std::ios::seekdir&) const { return __LINE__; }
+	constexpr std::size_t tellg(void) const { return 0; }
+	constexpr std::size_t seekg(int, const std::ios::seekdir& = std::ios::end) const { return 0; }
 };
-struct D { constexpr std::size_t size(void) const { return __LINE__; } };
+struct D { constexpr std::size_t size(void) const { return 0; } };
 struct AB : A, B {};
 struct CD : C, D {};
+struct BC : B, C {};
+struct AD : A, D {};
 struct Z {};
 
-int main(int argc, const char *argv[]) {
-	using std::cout;
-	using std::endl;
-	using std::ifstream;
-	using std::ofstream;
-	using std::ostringstream;
-	using std::string;
-	using std::pair;
-	using std::vector;
+struct Printer {
+	unsigned nameCol = 16, whichCol = 16, sizeCol = 4;
+	const char *pad = "  ";
+	template<class S>
+	S& header(S &s) {
+		return s << std::setw(nameCol) << "Description" << pad
+				<< std::setw(whichCol) << "Specialization" << pad
+				<< std::setw(sizeCol) << "Size" << pad
+				<< "(Real type)" << std::endl, s;
+	}
+	template<class S, class T>
+	S& operator()(S& s, unsigned line, std::string const& name, T && t) {
+		std::string realName = prettyTrunc<T>();
+		return s << std::setw(nameCol) << ((name.length() && name != realName) ? name : "") << pad
+			<< std::setw(whichCol) << whichSize(std::forward<T>(t)) << pad
+			<< std::setw(sizeCol) << size(std::forward<T>(t)) << pad
+			<< "(" << realName << ")" << std::endl, s;
+	}
+};
 
-	string buf = "Content in string";
-	ostringstream oss;
-	oss << "Content in ostringstream";
+
+template<class S, class T, unsigned N>
+S& dispatch(S& s, T const (&) [N]) { return s << __LINE__ << std::endl, s; }
+template<class S, class T>
+S& dispatch(S& s, T && t) { auto const& ct = t; return dispatch(s, ct); }
+int main(int argc, const char *argv[]) {
+	using namespace std;
+	using namespace Detail;
+
+	string str = "Content in string";
 
 	cout << "In file " << __FILE__ << "...\n";
 
 	ostringstream out;
-	auto prn = [&out] (unsigned line, const char *result, std::string prettied) {
-		out << prettied << " from line " << line << " -> " << result << "\n";
-	};
-	prn(__LINE__, size(A()), Pretty<A>());                         // calls length
-	prn(__LINE__, size(B()), Pretty<B>());                         // calls tellp
-	prn(__LINE__, size(C()), Pretty<C>());                         // calls tellg
-	prn(__LINE__, size(D()), Pretty<D>());                         // calls size
-	prn(__LINE__, size(Z()), Pretty<Z>());                         // calls fallback
-	prn(__LINE__, size(vector<int>()), Pretty<vector<int>>());     // calls size
-	prn(__LINE__, size(pair<int,int>()), Pretty<pair<int,int>>()); // calls fallback
-	prn(__LINE__, size(ifstream()), Pretty<ifstream>());           // calls tellg
-	prn(__LINE__, size(ofstream()), Pretty<ofstream>());           // calls tellp
+
+	// TODO determine why using 'arr[] = {}' (or E = int[0]) eliminates the template
+	// specialization for arrays
+	static constexpr unsigned N = 5;
+	typedef int X;
+	typedef const int Y;
+	typedef X E[N];
+	typedef add_const_t<E> EC;
+	//typedef const int EC[N];
+
+	E arr = {};
+	EC carr = {};
+	vector<int> vec = {0, 1, 2};
+	ostringstream oss("Hello world");
+	istringstream iss("Goodnight moon");
+	ostream &os = oss;
+	istream &is = iss;
+
+	typedef add_lvalue_reference_t<E> EL;
+	typedef add_rvalue_reference_t<E> ER;
+	typedef remove_reference_t<EL> NOREF_EL;
+	typedef remove_reference_t<ER> NOREF_ER;
+
+	typedef add_lvalue_reference_t<add_const_t<E>> ECL;
+	typedef add_const_t<add_rvalue_reference_t<ER>> ECR;
+	// same as add_rvalue_reference_t<add_const_t<ER>>
+
+	/*typedef add_const_t<remove_reference_t<ECL>> NOREF_ECL;
+	typedef add_const_t<remove_reference_t<ECR>> NOREF_ECR;*/
+	typedef remove_reference_t<ECL> NOREF_ECL;
+	typedef remove_reference_t<ECR> NOREF_ECR;
+	typedef remove_const_t<EC> NOC_EC;
+	typedef remove_const_t<ECL> NOC_ECL;
+	typedef remove_const_t<ECR> NOC_ECR;
+
+	typedef remove_reference_t<NOC_ECL> NOCREF_ECL;
+	typedef remove_reference_t<NOC_ECR> NOCREF_ECR;
+
+
+	cout << "Legend: C = const, L = lvalue reference, R = rvalue reference\n" << endl;
+	Printer printSize;
+	printSize.header(cout);
+	printSize(cout, __LINE__, "A (string-like)", A{});           // length
+	printSize(cout, __LINE__, "string", str);
+	printSize(cout, __LINE__, "string", std::move(str));
+	printSize(cout, __LINE__, "B (ostream-like)", B{});          // seekp/tellp
+	printSize(cout, __LINE__, "ostringstream", oss);             // fallback (!) TODO
+	printSize(cout, __LINE__, "ostream&", os);
+	printSize(cout, __LINE__, "C (istream-like)", C{});          // seekg/tellg
+	printSize(cout, __LINE__, "istringstream", iss);             // fallback (!) TODO
+	printSize(cout, __LINE__, "istream&", is);
+	//printSize(cout, __LINE__, "istringstream", std::move(iss));  // fallback (!) TODO
+
+	printSize(cout, __LINE__, "D (vector-like)", D{});           // size
+	printSize(cout, __LINE__, "vector", vec);                    // size
+	printSize(cout, __LINE__, "vector", std::move(vec));         // size
+
+	printSize(cout, __LINE__, "E", E{});                         // deduced &&
+	printSize(cout, __LINE__, "EC = E+C", EC{});                 // deduced const&
+	printSize(cout, __LINE__, "NOC_EC = EC-C", NOC_EC{});        // deduced
+	//                                                           // deduced &
+	printSize(cout, __LINE__, "EL = E+L", (EL) arr);
+	printSize(cout, __LINE__, "EL - L", NOREF_EL{});
+	printSize(cout, __LINE__, "ECL = EL+C", (ECL) arr);
+	printSize(cout, __LINE__, "ECL-C", (NOC_ECL)arr);
+	//                                                           // deduced &&
+	printSize(cout, __LINE__, "ECL-L", NOREF_ECL{});
+	printSize(cout, __LINE__, "ECL-C-L", NOCREF_ECL{});
+
+	//                                                           // deduced &&
+	printSize(cout, __LINE__, "ER = E+R", ER{});
+	printSize(cout, __LINE__, "ER - R", NOREF_ER{});
+	printSize(cout, __LINE__, "ECR = ER+C", ECR{});
+	printSize(cout, __LINE__, "ECR-C", NOC_ECR{});
+	printSize(cout, __LINE__, "ECR-R", NOREF_ECR{});
+	printSize(cout, __LINE__, "ECR-C-R", NOCREF_ECR{});
+	cout << endl;
+	cout << Pretty<E>() << " -> " << Pretty<Detail::RemoveCVRef_t<E>>() << endl;
+	cout << Pretty<EC>() << " -> " << Pretty<Detail::RemoveCVRef_t<EC>>() << endl;
+	cout << Pretty<EL>() << " -> " << Pretty<Detail::RemoveCVRef_t<EL>>() << endl;
+	cout << Pretty<ER>() << " -> " << Pretty<Detail::RemoveCVRef_t<ER>>() << endl;
+	cout << Pretty<ECL>() << " -> " << Pretty<Detail::RemoveCVRef_t<ECL>>() << endl;
+	cout << Pretty<ECR>() << " -> " << Pretty<Detail::RemoveCVRef_t<ECR>>() << endl;
+	/*cout << dispatch(cout, E{}) << endl;
+	cout << dispatch(cout, (EC) arr) << endl;
+	cout << dispatch(cout, (EL) arr) << endl;
+	cout << dispatch(cout, ER{}) << endl;
+	cout << dispatch(cout, (ECL) arr) << endl;*/
+
+	dispatch(cout, E{});
+	dispatch(cout, EC{});
+	dispatch(cout, (EL) arr);
+	dispatch(cout, ER{});
+	dispatch(cout, (ECL) arr);
+	//cout << dispatch(cout, ECR{}) << endl;
+	/*cout << "E + C = " << Pretty<EC>()
+			<< " or " << Pretty<add_const_t<E>>() << endl;
+	cout << "E + L = " << Pretty<E&>()
+			<< " or " << Pretty<add_lvalue_reference_t<E>>() << endl;
+	cout << "E + R = " << Pretty<E&&>()
+			<< " or " << Pretty<add_rvalue_reference_t<E>>() << endl;
+	cout << "E + C + L = " << Pretty<E const&>()
+			<< " or " << Pretty<add_lvalue_reference_t<add_const_t<E>>>() << endl;
+	cout << "E + C + R = " << Pretty<E const &&>()
+			<< " or " << Pretty<add_rvalue_reference_t<add_const_t<E>>>() << endl;
+	cout << "E + L + C = " << Pretty<E const&>()
+			// add_const_t<EL> = EL
+			<< " or " << Pretty<add_lvalue_reference_t<add_const_t<remove_reference_t<EL>>>>()
+			<< endl;
+	cout << "E + R + C = " << Pretty<E const &&>()
+			<< " or " << Pretty<add_const_t<add_rvalue_reference_t<E>>>() << endl;
+	cout << Pretty<add_lvalue_reference_t<add_rvalue_reference_t<E>>>() << " / "
+			<< Pretty<add_rvalue_reference_t<add_lvalue_reference_t<E>>>() << endl;
+	cout << Pretty<add_lvalue_reference_t<add_rvalue_reference_t<EC>>>() << " / "
+			<< Pretty<add_rvalue_reference_t<add_lvalue_reference_t<EC>>>() << endl;
+	cout << Pretty<add_const_t<add_lvalue_reference_t<add_rvalue_reference_t<E>>>>() << " / "
+			<< Pretty<add_const_t<add_rvalue_reference_t<add_lvalue_reference_t<E>>>>() << endl;*/
+	/*cout << boolalpha;
+	cout << Pretty<int[]>() << " : " << HasDimension<int[]>::value << endl;
+	cout << Pretty<int (&) []>() << " : " << HasDimension<int (&) []>::value << endl;
+	cout << Pretty<int const (&) []>() << " : " << HasDimension<int const (&) []>::value << endl;
+	cout << Pretty<int (&&) []>() << " : " << HasDimension<int (&&) []>::value << endl;
+
+	static_assert(std::is_same<const XA, YA>::value, "Inner/outer const");
+	static_assert(std::is_same<std::remove_reference_t<std::add_const_t<XA>>, YA>::value, "");
+	static_assert(std::is_same<std::remove_const_t<YA>, XA>::value, "");
+	//static_assert(std::is_same<YA&, std::add_const_t<XA>>::value, "");
+	cout << Pretty<std::add_const_t<XA>>() << " vs " << Pretty<YA&>() << endl;*/
+
+
+	printSize(cout, __LINE__, "Z (empty)", Z());                 // fallback (ok)
+	printSize(cout, __LINE__, "", pair<int,int>());              // fallback (ok)
 	cout << out.str();
 
 }
