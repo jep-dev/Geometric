dc9e63f740e1c2932b44b4a18145bc659377ea70
John Petersen <jepetersen.dev@gmail.com>
2018-11-13 18:20:04 -0500

    Added tests of RepeatSeq and PartialSumSeq.

--- app/sequence.cpp
+++ app/sequence.cpp
@@ -9,11 +9,24 @@ S& testBinomial(S &s) { return s; }
 
 template<class S, unsigned N, unsigned... IN>
 S& testBinomial(S &os) {
-	os << "Binomial coefficients (" << N << "): ";
+	os << N << ". ";
 	endl(os << Binomial<N>());
 	return testBinomial<S, IN...>(os);
 }
 
 int main(int argc, const char *argv[]) {
-	testBinomial<std::ostream&, 0, 1, 2, 3, 4, 5, 6>(std::cout);
+	using namespace std;
+	using namespace Detail;
+	// The executable size is independent of N, which should mean that the sequences
+	// all disappear in abstract elimination.
+	static constexpr unsigned N = 10;
+	typedef RepeatSeq<N, unsigned, 1> ones_t;
+	typedef PartialSumSeq<ones_t> inc_t;
+	typedef PartialSumSeq<inc_t> inc2_t;
+	cout << "10 repeats of 1: " << ones_t{} << endl;
+	cout << "1st partial sum: " << inc_t{} << endl;
+	cout << "2nd partial sum: " << inc2_t{} << endl;
+	cout << endl;
+	cout << "Binomial coefficients: " << endl;
+	testBinomial<std::ostream&, 0, 1, 2, 3, 4, 5, 6>(cout);
 }
