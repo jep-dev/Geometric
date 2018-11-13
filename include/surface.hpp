e77107bb7e67ea81865370287d3ffe9c100ca973
John Petersen <jepetersen.dev@gmail.com>
2018-11-13 15:46:19 -0500

    Modified 'surface' to emplace UV params separately and return the right size; added surface2 (quadratic dual quaternion Bezier) and surface3 (quadratic point Bezier); in progress.

--- include/surface.hpp
+++ include/surface.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 ///@endcond
 
+#include "bezier.hpp"
 #include "quaternion.hpp" // Quaternion
 #include "dual.hpp" // Dual
 #include "point.hpp" // Point
@@ -134,12 +135,72 @@ unsigned surface(V<S,VT...> & vertices, W<T,WT...> & indices,
 			U t = U(j)/(N-1);
 			Dq xform = sclerp(nw, ne, sw, se, s, t);
 			auto p2 = (p ^ xform) + center;
-			emplace_vertices(vertices, p2, s, t);
-			if(i && j)
+			emplace_vertices(vertices, p2);
+			if(uv) emplace_vertices(vertices, s, t);
+			if(i && j) {
 				emplace_indices(indices, I+j, I-N+j-1, I+j-1, I-N+j-1, I+j, I-N+j);
+			}
 		}
 	}
 	return offset + M*N;
+}
+
+template<class V, class W, class U>
+unsigned surface2(V & vertices, W & indices,
+		const DualQuaternion<U> (&rowMajor)[3][3], Point<U> const& p, Point<U> const& center,
+		unsigned M = 100, unsigned N = 100, unsigned offset = 0, bool uv = true) {
+	typedef DualQuaternion<U> Dq;
+	typedef Point<U> Pt;
+	auto const& r0 = rowMajor[0], r1 = rowMajor[1], r2 = rowMajor[2];
+	for(unsigned i = 0, I = offset; i < M; i++, I += N) {
+		U s = U(i)/(M-1);
+		for(unsigned j = 0; j < N; j++) {
+			U t = U(j)/(N-1);
+			//auto p2 = center + (r0[0]*(1-s) + r0[1]*s) * (1-t) + (r1[0]*(1-s) + r1[1]*s) * t;
+			//auto p2 = center + linear(linear(r0[0], r0[1], s), linear(r1[0], r1[1], s), t);
+			//auto p2 = center + linear(r0[0], r0[1], r1[0], r1[1], s, t);
+			//auto p2 = center + quadratic(r0[0], r0[1], r0[2], r1[0], r1[1], r1[2],
+					//r[2][0], r[2][1], r[2][2], s, t);
+			//auto p2 = center + (p ^ linear(r0[0], r0[1], r0[2], r1[0], r1[1], r1[2], r2[0], r2[1], r2[2], s, t));
+			auto p2 = center + (p ^ sclerp(
+				sclerp(r0[0], r0[1], r1[0], r1[1], s, t),
+				sclerp(r0[1], r0[2], r1[1], r1[2], s, t),
+				sclerp(r1[0], r1[1], r2[0], r2[1], s, t),
+				sclerp(r1[1], r1[2], r2[1], r2[2], s, t), s, t));
+
+			emplace_vertices(vertices, p2);
+			if(uv) emplace_vertices(vertices, s, t);
+			if(i && j) {
+				emplace_indices(indices, I-N+j-1, I+j, I+j-1, I-N+j-1, I-N+j, I+j);
+			}
+		}
+	}
+	return offset + M*N;
+}
+template<class V, class W, class U>
+unsigned surface3(V & vertices, W & indices,
+		const Point<U> (&rowMajor)[3][3], Point<U> const& center,
+		unsigned M = 100, unsigned N = 100, unsigned offset = 0, bool uv = true) {
+	typedef DualQuaternion<U> Dq;
+	typedef Point<U> Pt;
+	auto const& r0 = rowMajor[0], r1 = rowMajor[1], r2 = rowMajor[2];
+	for(unsigned i = 0, I = offset; i < M; i++, I += N) {
+		U s = U(i)/(M-1), s2 = s*s, S = 1-s, S2 = S*S, Ss = S*s;
+		for(unsigned j = 0; j < N; j++) {
+			U t = U(j)/(N-1), t2 = t*t, T = 1-t, T2 = T*T, Tt = T*t;
+
+			auto pt = center +
+				(((r0[0]*S + r0[1]*s)*S + r0[2]*s2)*T
+				+((r1[0]*S + r1[1]*s)*S + r1[2]*s2)*2*t)*T
+				+((r2[0]*S + r2[1]*s)*S + r2[2]*s2)*t*t;
+
+			emplace_vertices(vertices, pt);
+			if(uv) emplace_vertices(vertices, s, t);
+			if(i && j) {
+				emplace_indices(indices, I-N+j-1, I+j, I+j-1, I-N+j-1, I-N+j, I+j);
+			}
+		}
+	}
 }
 
 #endif
