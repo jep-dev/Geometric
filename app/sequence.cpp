e7734ba2c4a586efa5f9f4bb0680626733a1ee9e
John Petersen <jepetersen.dev@gmail.com>
2018-11-13 17:00:07 -0500

    Added use of testBinomial (somehow missed that) and a terminal case (empty binomial sequence).

--- app/sequence.cpp
+++ app/sequence.cpp
@@ -5,16 +5,15 @@
 #include "binomial.hpp"
 
 template<class S>
-void testBinomial(S &s) { return s; }
+S& testBinomial(S &s) { return s; }
 
 template<class S, unsigned N, unsigned... IN>
 S& testBinomial(S &os) {
+	os << "Binomial coefficients (" << N << "): ";
 	endl(os << Binomial<N>());
 	return testBinomial<S, IN...>(os);
 }
 
 int main(int argc, const char *argv[]) {
-	/*
-	 * Seq<I...> -> Tag<Binomial<I>::value_type...>
-	 */
+	testBinomial<std::ostream&, 0, 1, 2, 3, 4, 5, 6>(std::cout);
 }
