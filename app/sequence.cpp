c1513cfc89665bdcc7461c4b27ab8c050f118060
John Petersen <jepetersen.dev@gmail.com>
2018-11-14 00:30:50 -0500

    Added ProductSeq test; reformatted output to show more clearly what derives from what

--- app/sequence.cpp
+++ app/sequence.cpp
@@ -21,16 +21,17 @@ int main(int argc, const char *argv[]) {
 	// all disappear in abstract elimination.
 	static constexpr unsigned N = 10, I = 4;
 	typedef RepeatSeq<N, unsigned, 1> ones_t; // {1, 1, ..., 1, 1}
-	//typedef SumSeq<ones_t> sum_t;
 	typedef SumSeq<typename ones_t::value_type> sum_t;
 	typedef PartialSumSeq<ones_t> inc_t;      // {1, 2, ..., 9, 10}
+	typedef ProductSeq<typename inc_t::value_type> product_t;
 	typedef PartialSumSeq<typename inc_t::value_type> inc2_t;      // {1, 3, ..., 45, 55}
 
-	cout << "10 repeats of 1: " << ones_t{} << endl;
-	cout << "1st partial sum: " << inc_t{} << endl;
-	cout << "        1st sum: " << sum_t{} << endl;
-	cout << "2nd partial sum: " << inc2_t{} << endl;
-	cout << "    Nth for N=" << I << ": " << inc2_t::nth<I>() << endl;
+	cout << "10 1's: " << ones_t{} << endl;
+	cout << "  - Sum: " << sum_t{} << endl;
+	cout << "  - Partial sum: " << inc_t{} << endl;
+	cout << "    - Product: " << product_t{} << endl;
+	cout << "    - Partial sum: " << inc2_t{} << endl;
+	cout << "    - Nth for N=" << I << ": " << inc2_t::nth<I>() << endl;
 	cout << '\n';
 
 	cout << "Binomial coefficients: " << endl;
