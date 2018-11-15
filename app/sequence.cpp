93a2d56edb415a5e41f346afbecb48bcb0269cf0
John Petersen <jepetersen.dev@gmail.com>
2018-11-15 12:53:09 -0500

    Increased binomial outputs to investigate a pattern: in a triangular 2D array, where row <= column, the indices of each row's 0th column in the corresponding 1D array is the the 2nd column of binomial coefficients (N+1 choose 2, beginning with 1 choose 2 = 0).

--- app/sequence.cpp
+++ app/sequence.cpp
@@ -34,6 +34,9 @@ int main(int argc, const char *argv[]) {
 	cout << "    - Nth for N=" << I << ": " << inc2_t::nth<I>() << endl;
 	cout << '\n';
 
+	cout << "Fibonacci<10> = " << Fibonacci<10>::value << endl;
+	cout << '\n';
+
 	cout << "Binomial coefficients: " << endl;
-	testBinomial<std::ostream&, 0, 1, 2, 3, 4, 5, 6>(cout);
+	testBinomial<std::ostream&, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9>(cout);
 }
