f6e0e00cff1a5a45f16516df72c0a727a5e0ea0e
John Petersen <jepetersen.dev@gmail.com>
2018-11-15 21:30:48 -0500

    Automated more tests and tabulated the output; now expressions, their simplified forms, and derivatives print on each line.

--- app/expression.cpp
+++ app/expression.cpp
@@ -26,18 +26,59 @@ int main(int argc, const char *argv[]) {
 	using T = float;
 	using DQ = DualQuaternion<T>;
 
+	SharedSymbol<DQ> u = make_variable<DQ>('u'),
+		v = make_variable<DQ>('v'),
+		w = make_scalar<DQ>(DQ(1_e));
+	vector<SharedSymbol<DQ>> symbols = {
+		u, v, w, make_exp(u), make_exp(v), make_exp(w),
+		make_product(u, u), make_product(v, v), make_product(w, w),
+		make_product(u, v), make_product(u, w), make_product(v, w),
+		make_sum(u, u), make_sum(u, v), make_sum(u, w),
+		make_sum(u, u), make_sum(v, v), make_sum(w, w)
+	};
+	unsigned N = symbols.size();
+	vector<string> lines(N+1, "");
+	lines[0] = "Expression";
+	ostringstream oss;
+	for(unsigned i = 0; i < N; i++) {
+		oss << *symbols[i];
+		lines[i+1] += oss.str();
+		oss.str("");
+	}
+	Streams::level_insert(lines, "  ");
+
+	lines[0] += "Reduced";
+	for(unsigned i = 0; i < N; i++) {
+		oss << *symbols[i] -> simplify();
+		lines[i+1] += oss.str();
+		oss.str("");
+	}
+	Streams::level_insert(lines, "  ");
+	for(char ch : {'u', 'v'}) {
+		lines[0] += string("d/d") + ch;
+		for(unsigned i = 0; i < N; i++) {
+			oss << *(symbols[i] -> derive(ch));
+			lines[i+1] += oss.str();
+			oss.str("");
+		}
+		Streams::level_insert(lines, "  ");
+	}
+	for(auto const& line : lines)
+		cout << line << endl;
+
 	/*std::map<char, DQ> vars = {
 		{'u', DQ{1}}
 	};*/
 
-	SharedSymbol<DQ> u = make_variable<DQ>('u'),
+	/*SharedSymbol<DQ> u = make_variable<DQ>('u'),
 		v = make_variable<DQ>('v'),
 		w = make_scalar<DQ>(DQ(1_e)),
+		eu = make_exp(u), ev = make_exp(v),
 		uu = make_product(u, u), vv = make_product(v, v), ww = make_product(w, w),
 		uv = make_product(u, v), uw = make_product(u, w), vw = make_product(v, w),
 		u_u = make_sum(u, u), u_v = make_sum(u, v), u_w = make_sum(u, w),
 		u2 = make_sum(u, u), v2 = make_sum(v, v), w2 = make_sum(w, w);
-	for(auto const& expr : {u, v, w, uu, uv, vv, ww, uv, uw, vw,
+	for(auto const& expr : {u, v, w, eu, ev, uu, uv, vv, ww, uv, uw, vw,
 			u_u, u_v, u_w, u2, v2, w2})
-		print_derivatives(cout, expr, 'u', 'v');
+		print_derivatives(cout, expr, 'u', 'v');*/
 }
