b71f63152f77a503520c70bddedd167e989c363d
John Petersen <jepetersen.dev@gmail.com>
2018-12-11 11:25:05 -0500

    Updated to use alias for size_t, changed formatting

--- app/sequence.cpp
+++ app/sequence.cpp
@@ -3,28 +3,30 @@
 //#include <sstream>
 
 #include "binomial.hpp"
+#include "dual.hpp"
+
+using namespace Detail;
 
 template<class S>
 S& testBinomial(S &s) { return s; }
 
-template<class S, unsigned N, unsigned... IN>
-S& testBinomial(S &os) {
+template<class S, Size N, Size... I>
+S& testBinomial(S &os, SeqSz<N, I...> = {}) {
 	os << N << ". ";
 	endl(os << Binomial<N>());
-	return testBinomial<S, IN...>(os);
+	return testBinomial<S, I...>(os);
 }
 
 int main(int argc, const char *argv[]) {
 	using namespace std;
-	using namespace Detail;
 	// The executable size is independent of N, which should mean that the sequences
 	// all disappear in abstract elimination.
 	static constexpr unsigned N = 10, I = 4;
-	typedef RepeatSeq<N, unsigned, 1> ones_t; // {1, 1, ..., 1, 1}
-	typedef SumSeq<typename ones_t::value_type> sum_t;
-	typedef PartialSumSeq<ones_t> inc_t;      // {1, 2, ..., 9, 10}
-	typedef ProductSeq<typename inc_t::value_type> product_t;
-	typedef PartialSumSeq<typename inc_t::value_type> inc2_t;      // {1, 3, ..., 45, 55}
+	typedef RepeatSeq<N, unsigned, 1> ones_t;          // {1, 1, ..., 1, 1}
+	typedef SumSeq<ValueType_t<ones_t>> sum_t;
+	typedef PartialSumSeq<ones_t> inc_t;               // {1, 2, ..., 9, 10}
+	typedef ProductSeq<ValueType_t<inc_t>> product_t;
+	typedef PartialSumSeq<ValueType_t<inc_t>> inc2_t;  // {1, 3, ..., 45, 55}
 
 	cout << "10 1's: " << ones_t{} << endl;
 	cout << "  - Sum: " << sum_t{} << endl;
@@ -38,5 +40,5 @@ int main(int argc, const char *argv[]) {
 	cout << '\n';
 
 	cout << "Binomial coefficients: " << endl;
-	testBinomial<std::ostream&, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9>(cout);
+	testBinomial<ostream&, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9>(cout) << endl;
 }
