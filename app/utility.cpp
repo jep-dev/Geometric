1b1cef983d1e96b7e70a24fe44ff002a98b63ddf
John Petersen <jepetersen.dev@gmail.com>
2018-12-15 15:54:30 -0500

    Made aliases for ValueType with and without recursion, and variants for sum/difference/etc.; added operators between wrapped types to distribute the operation; added tests of operators and new type combinations

--- app/utility.cpp
+++ app/utility.cpp
@@ -20,8 +20,8 @@ S& print_valueTypes(S& s, T const& rhs) {
 	using namespace Detail;
 	using namespace std;
 	s << INDENT << prettyTrunc<T>();
-	typedef ValueType_t<false, T> VT_T;
-	typedef ValueType_t<true, T> VTR_T;
+	typedef Value_t<false, T> VT_T;
+	typedef Value_t<true, T> VTR_T;
 	if(!is_same<T, VTR_T>::value) {
 		s << ", contains ";
 		if(!is_same<T, VT_T>::value) {
@@ -36,19 +36,19 @@ S& print_valueTypes(S& s, T const& rhs) {
 	}
 	return s << std::endl, s;
 	/*s << ", contains shallow ";
-	s << prettyTrunc<ValueType_t<false, T>>() << " and deep "
-	return print_types(s, rhs, ValueType_t<false, T>(), ValueType_t<true, T>());*/
+	s << prettyTrunc<Value_t<false, T>>() << " and deep "
+	return print_types(s, rhs, Value_t<false, T>(), Value_t<true, T>());*/
 }
 template<class S, class T, class U>
 std::ostream& print_sumValueTypes(S &s, T const& t, U const& u) {
 	using namespace Detail;
 	typedef SumType_t<T, U> TU;
-	typedef ValueType_t<false, T> VT_T;
-	typedef ValueType_t<false, U> VT_U;
-	typedef ValueType_t<false, TU> VT_TU;
-	typedef ValueType_t<true, T> VTR_T;
-	typedef ValueType_t<true, U> VTR_U;
-	typedef ValueType_t<true, VT_TU> VTR_TU;
+	typedef Value_t<false, T> VT_T;
+	typedef Value_t<false, U> VT_U;
+	typedef Value_t<false, TU> VT_TU;
+	typedef Value_t<true, T> VTR_T;
+	typedef Value_t<true, U> VTR_U;
+	typedef Value_t<true, VT_TU> VTR_TU;
 	s << "Sums of types...\n";
 	print_types<S, T, VT_T, VTR_T>(s << INDENT << "T: ") << "\n";
 	print_types<S, U, VT_U, VTR_U>(s << INDENT << "T: ") << "\n";
@@ -62,48 +62,91 @@ std::ostream& print_sumValueTypes(S &s, T const& t, U const& u) {
 }
 
 template<class T> struct T1 { typedef T value_type; };
-template<class T> struct T2 { typedef T1<T> value_type; };
-template<class T> struct T3 : T1<T> { };
+template<class T> using T2 = T1<T1<T>>;
+//template<class T> struct T2 { typedef T1<T> value_type; };
+// template<class T> struct T3 : T1<T> { };
+
+/*template<class S, class T>
+auto operator+(S const& lhs, T const& rhs)
+	-> T3<T1<Detail::SumValue_t<true, S, T>>> { return {}; }*/
+
+template<class T> using VAL = Detail::Value_t<false, T>;
+template<class T> using VALR = Detail::Value_t<true, T>;
+template<class S, class T> using ADD = Detail::SumType_t<S,T>;
+template<class S, class T> using ADDR = Detail::SumValue_t<true, S, T>;
+template<class S, class T> using SUB = Detail::DifferenceType_t<S,T>;
+template<class S, class T> using SUBR = Detail::DifferenceValue_t<true, S,T>;
+template<class S, class T> using MUL = Detail::ProductType_t<S,T>;
+template<class S, class T> using MULR = Detail::ProductValue_t<true, S,T>;
+template<class S, class T> using DIV = Detail::QuotientType_t<S,T>;
+template<class S, class T> using DIVR = Detail::QuotientValue_t<true, S,T>;
 
 template<class S, class T>
-auto operator+(S const& lhs, T const& rhs)
-	-> T3<T1<Detail::SumValueType_t<true, S, T>>> { return {}; }
+auto operator+(T1<S> const&, T1<T> const&)
+		-> T1<ADD<S,T>> { return {}; }
+template<class S, class T>
+auto operator-(T1<S> const&, T1<T> const&)
+		-> T1<SUB<S,T>> { return {}; }
+template<class S, class T>
+auto operator*(T1<S> const&, T1<T> const&)
+		-> T1<MUL<S,T>> { return {}; }
+template<class S, class T>
+auto operator/(T1<S> const&, T1<T> const&)
+		-> T1<DIV<S,T>> { return {}; }
+
 
 int main(int argc, const char *argv[]) {
 	using namespace Detail;
 	using namespace std;
 
-	typedef float F;
-	typedef double D;
-	typedef T1<F> F1; typedef T2<F> F2;
-	typedef ValueType_t<false, F1> VT_F1;
-	typedef ValueType_t<false, F2> VT_F2;
-	typedef ValueType_t<true, F1> VTR_F1;
-	typedef ValueType_t<true, F2> VTR_F2;
-	typedef SumValueType_t<true, F1, F2> VTR_F;
-	static_assert(is_same<VT_F2, F1>::value,
-			"Without recursion, ValueType_t must yield the outermost value_type");
-	static_assert(is_same<VTR_F1, F>::value && is_same<VTR_F2, F>::value,
-			"With recursion, ValueType_t must yield the innermost value_type");
-	static_assert(is_same<VTR_F, F>::value,
-			"With recursion, SumValueType_t must yield the sum type of innermost value_types");
+
+	typedef int I; typedef T1<I> I1; typedef T2<I> I2;
+	typedef float F; typedef T1<F> F1; typedef T2<F> F2;
+
+	static_assert(is_same<VAL<I2>, I1>::value && is_same<VAL<I1>, I>::value,
+		"Value_t is the outermost value_type");
+	static_assert(is_same<VALR<I1>, I>::value && is_same<VALR<I2>, I>::value,
+		"Recursive Value_t is the innermost value_type");
+
+	static_assert(is_same<ADD<I,F>,F>::value,
+		"SumType_t is the sum of outermost value_types");
+	static_assert(is_same<decltype(declval<I1>()+declval<F1>()), F1>::value,
+		"T1<S> + T1<T> = T1<S+T>");
+	static_assert(is_same<decltype(declval<I2>()+declval<F2>()), T1<F1>>::value,
+		"T1<T1...<S>> + T1<T1...<T>> = T1<T1...<S+T>>");
+		// = T1<T1...<S> + T1...<T>> = T1<T1...<S+T>>
+	// T1...<S> + T1<T1...<T>> is only defined if S = T1<S'>
+	static_assert(is_same<ADDR<I1,F1>, ADDR<I2,F2>>::value,
+		"Recursive SumType_t is the sum of innermost value_types");
+
+	static_assert(is_same<SUB<I,F>,F>::value,
+		"DifferenceType_t is the sum of outermost value_types");
+	static_assert(is_same<decltype(declval<I1>()-declval<F1>()), F1>::value,
+		"T1<S> - T1<T> = T1<S-T>");
+	static_assert(is_same<decltype(declval<I2>()-declval<F2>()), T1<F1>>::value,
+		"T1<T1...<S>> - T1<T1...<T>> = T1<T1...<S-T>>");
+	static_assert(is_same<SUBR<I1,F1>, SUBR<I2,F2>>::value,
+		"Recursive DifferenceType_t is the sum of innermost value_types");
+
+	static_assert(is_same<MUL<I,F>,F>::value,
+		"ProductType_t is the sum of outermost value_types");
+	static_assert(is_same<decltype(declval<I1>()*declval<F1>()), F1>::value,
+		"T1<S> * T1<T> = T1<S*T>");
+	static_assert(is_same<decltype(declval<I2>()*declval<F2>()), T1<F1>>::value,
+		"T1<T1...<S>> * T1<T1...<T>> = T1<T1...<S*T>>");
+	static_assert(is_same<MULR<I1,F1>, MULR<I2,F2>>::value,
+		"Recursive ProductType_t is the sum of innermost value_types");
+
+	static_assert(is_same<DIV<I,F>,F>::value,
+		"QuotientType_t is the sum of outermost value_types");
+	static_assert(is_same<decltype(declval<I1>()/declval<F1>()), F1>::value,
+		"T1<S> / T1<T> = T1<S/T>");
+	static_assert(is_same<decltype(declval<I2>()/declval<F2>()), T1<F1>>::value,
+		"T1<T1...<S>> / T1<T1...<T>> = T1<T1...<S/T>>");
+	static_assert(is_same<DIVR<I1,F1>, DIVR<I2,F2>>::value,
+		"Recursive QuotientType_t is the sum of innermost value_types");
 
 
-	typedef T1<D> D1;
-	typedef T2<D> D2;
-	/*typedef ValueType_t<false, D1> VT_D1;
-	typedef ValueType_t<false, D2> VT_D2;
-	typedef ValueType_t<true, D1> VTR_D1;
-	typedef ValueType_t<true, D2> VTR_D2;
-	//typedef SumValueType_t<false, D1, D2> VT_D;
-	typedef SumValueType_t<true, D1, D2> VTR_D;
-	static_assert(is_same<VT_D1, D>::value && is_same<VT_D2, D>::value,
-			"Without recursion, SumValueType_t must yield the outermost value_type");
-	static_assert(is_same<VTR_D1, D>::value && is_same<VTR_D2, D>::value,
-			"With recursion, SumValueType_t must yield the innermost value_type");
-	typedef SumValueType_t<F1, F2, D1, D2> VT_FD;
-	static_assert(is_same<VT_D, D>::value && is_same<VT_FD, D>::value,
-			"With recursion, SumValueType_t must yield the sum type of innermost value_types");*/
-	print_sumValueTypes(cout, F1{}, D1{}) << endl;
-	print_sumValueTypes(cout, F2{}, D2{}) << endl;
+	print_sumValueTypes(cout, I1{}, F1{}) << endl;
+	print_sumValueTypes(cout, I2{}, F2{}) << endl;
 }
