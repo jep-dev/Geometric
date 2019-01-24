9fbec5d449e2f37735265d46d7a7f8f8ae1448f3
John Petersen <jepetersen.dev@gmail.com>
2019-01-24 13:39:38 -0500

    Added parse_value here

--- include/parse.hpp
+++ include/parse.hpp
@@ -1,15 +1,51 @@
 #include "streams.hpp"
+#include "numeric.hpp"
 
 namespace Streams {
 
-template<class S, class T>
+/*template<class S, class T>
 auto parse_value(S const& s, T const& def)
 		-> std::enable_if_t<std::is_arithmetic<T>::value, T> {
 	istringstream iss(s);
 	T local = def;
 	iss >> local;
 	return iss.fail() ? def : local;
-}
+}*/
+
+template<class S = float, class T = const char*>
+S parse_value(T const& word, S def = make_NaN<S>());
+/*template<class T = float, class S = std::string>
+T parse_value(S const& word) {
+	T out = make_NaN<T>();
+	std::string value;
+	bool neg = false, major = false, minor = false, pt = false;
+	char prev = '\0';
+	for(auto c : word) {
+		// TODO better criteria for value characters?
+		if(std::isdigit(c)) {
+			// if(!major && c == '0') ??
+			if(pt) minor = true;
+			else major = true;
+			value += c;
+		} else if(c == '.') {
+			if(pt) return out;
+			pt = major = true;
+			value += c;
+		} else if(c == '-') {
+			if(major) return out;
+			neg = !neg;
+		} else break;
+	}
+	if(pt && !minor || value == "")
+		return make_NaN<T>();
+	if(neg && value == "") return -1;
+	std::istringstream iss(value);
+	iss >> out;
+	if(iss.fail())
+		return make_NaN<T>();
+	return neg ? -out : out;
+}*/
+
 template<class S, class T>
 auto parse_positive(S const& s, T const& def)
 		-> std::enable_if_t<std::is_arithmetic<T>::value, T> {
