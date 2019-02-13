519b70c13c2f694ecd9d455d266cb4b40e93c1b4
John Petersen <jepetersen.dev@gmail.com>
2019-02-13 14:03:41 -0500

    Added isSame to abbreviate decltypes in is_same; replaced several exhaustive tests with functional equivalents

--- app/tag.cpp
+++ app/tag.cpp
@@ -1,9 +1,11 @@
 #include <array>
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <vector>
 
-#include "functional.hpp"
+#include "functional/for_each.hpp"
+#include "functional/transform.hpp"
 #include "pretty.hpp"
 #include "tag.hpp"
 
@@ -18,6 +20,7 @@ struct Base {
 };
 
 struct u; struct u : Base<u> { using Base::operator=; };
+struct u2; struct u2 : Base<u2> { using Base::operator=; };
 struct v; struct v : Base<v> { using Base::operator=; };
 struct w; struct w : Base<w> { using Base::operator=; };
 struct x; struct x : Base<x> { using Base::operator=; };
@@ -29,16 +32,37 @@ template<class S, class T>
 S& operator<<(S &s, Base<T> const& b) {
 	return s << Pretty<T>() << '(' << b.value << ')';
 }
-
-int main(int argc, const char *argv[]) {
-	using std::cout;
-	using std::endl;
-	using std::pair;
-	using std::string;
-	using std::array;
-	using std::vector;
+template<class S, class T, class QUAL = std::true_type>
+constexpr bool isSame(S && s, T && t, QUAL = {}) {
+	return std::is_same<S, T>::value;
+}
+template<class S, class T>
+constexpr bool isSame(S && s, T && t, std::false_type) {
+	return std::is_same<Detail::RemoveCVRef_t<S>, Detail::RemoveCVRef_t<T>>::value;
+}
+template<class... C>
+constexpr bool all_same(C &&... c) {
 	using namespace Detail;
+	typedef decltype(cartesian<std::is_same>(Tag<C...>{}, Tag<C...>{})) sames_t;
+	typedef decltype(tag_value_to_seq(sames_t{})) sames_v;
+	return boolAndSeq(sames_v{});
+}
 
+template<class L, class R> using Shift_t = decltype(L{} << R{});
+
+/*struct PrintSimple {
+	template<class OS>
+	OS& operator()(OS & os
+};*/
+struct PrintShift {
+	template<class I, class U, class OS>
+	void operator()(I const& l, U const& r, OS &os) const {
+		os << "  " << pretty<U>() << " << " << std::showpos << I::value
+			<< " = " << pretty(r << l) << ';' << std::endl;
+	}
+};
+
+namespace Detail {
 	using E = Tag<>;
 	using U = Tag<u>;
 	using V = Tag<v>;
@@ -54,59 +78,75 @@ int main(int argc, const char *argv[]) {
 		using XYZ = Tag<x, y, z>;
 	using F = Tag<float>;
 	using D = Tag<double>;
+}
+
+int main(int argc, const char *argv[]) {
+	using std::cout;
+	using std::endl;
+	using std::pair;
+	using std::string;
+	using std::array;
+	using std::vector;
+	using std::tuple;
+	using std::make_tuple;
+	using namespace Detail;
 
 	string indent = "  ", para = "\n" + indent;
 
 	std::ostringstream oss;
-	oss << "Basic types: " << para
-			<< "0 = " << pretty(E{}) << ";" << para
-			<< "U = " << pretty(U{}) << "; "
-			<< "V = " << pretty(V{}) << "; "
-			<< "W = " << pretty(W{}) << ";" << para
-			<< "X = " << pretty(X{}) << "; "
-			<< "Y = " << pretty(Y{}) << "; "
-			<< "Z = " << pretty(Z{}) << ".\n";
+	tuple<E, U, V, W, X, Y, Z> types;
+	auto type_names = make_tuple("  0", "\n  U", "V", "W", "\n  X", "Y", "Z");
+	auto type_strs = transform(types, [] (auto && t) {
+		return pretty<RemoveCVRef_t<decltype(t)>>(); });
 
-	static_assert(std::is_same<decltype(E{}+E{}), E>::value
-			&& std::is_same<decltype(E{}+X{}), X>::value
-			&& std::is_same<decltype(U{}+E{}), U>::value,
+	oss << "Basic types:" << endl;
+	for_each_zip(type_names, type_strs, [] (auto && l, auto && r, auto && os)
+			{ os << l << " = " << r << "; "; }, oss);
+	oss << endl;
+
+	static_assert(isSame(E{}+E{}, E{}) && isSame(E{}+X{}, X{}) && isSame(U{}+E{}, U{}),
 		"The empty tag must be the additive identity");
 	oss << "Direct sums (operator+):" << para
 			<< "0+0 = " << pretty(E{}+E{}) << "; "
 			<< "U+0 = " << pretty(U{}+E{}) << "; "
 			<< "0+X = " << pretty(E{}+X{}) << ";" << para;
 
-	static_assert(std::is_same<decltype(UV{}+W{}), decltype(U{}+VW{})>::value
-			&& std::is_same<decltype(XY{}+Z{}), decltype(X{}+YZ{})>::value,
+	static_assert(isSame(UV{}+W{}, U{}+VW{}) && isSame(XY{}+Z{}, X{}+YZ{}),
 		"Operator+ must be associative");
 	oss << "(U+V)+W = U+(V+W) = " << pretty((U{}+V{})+W{}) << " (associative.)" << para;
 
-	static_assert(std::is_same<decltype(V{}+U{}), decltype(reverse(UV{}))>::value,
+	static_assert(isSame(V{}+U{}, reverse(UV{})),
 		"Operator+ must preserve order (non-commutative)");
-	oss << "U+V = " << pretty(U{}+V{})
-				<< " != V+U = " << pretty(V{}+U{}) << " (non-commutative!)\n";
+	oss << "U+V = " << pretty(U{}+V{}) << " != "
+			"V+U = " << pretty(V{}+U{}) << " (non-commutative!)\n";
 
-	static_assert(std::is_same<UV, decltype(U{}+V{})>::value
-			&& std::is_same<VW, decltype(V{}+W{})>::value
-			&& std::is_same<UVW, decltype(U{}+V{}+W{})>::value
-			&& std::is_same<XY, decltype(X{}+Y{})>::value
-			&& std::is_same<YZ, decltype(Y{}+Z{})>::value
-			&& std::is_same<XYZ, decltype(X{}+Y{}+Z{})>::value,
+	static_assert(isSame(UV{}, U{}+V{}) && isSame(VW{}, V{}+W{})
+			&& isSame(UVW{}, U{}+V{}+W{}) && isSame(XY{}, X{}+Y{})
+			&& isSame(YZ{}, Y{}+Z{}) && isSame(XYZ{}, X{}+Y{}+Z{}),
 		"Operator+ must be direct sum of types (treated as orthogonal)");
 	oss << "Inner product (operator*):" << para
 			<< "0*0 = " << pretty(E{}*E{}) << ";" << para
 			<< "U*X = " << pretty(U{}*X{}) << ";" << para
 			<< "(U+V) * (X+Y) = " << pretty(UV{}) << " * " << pretty(XY{})
 				<< " = " << pretty(UV{}*XY{}) << ";" << para;
-	static_assert(std::is_same<decltype(U{}*X{}+V{}*Y{}), decltype(UV{}*XY{})>::value,
+	static_assert(isSame(U{}*X{}+V{}*Y{}, UV{}*XY{}),
 		"Operator* must be distributive over operator+");
 	oss << "U*X+V*Y = (U+V)*(X+Y) = " << pretty(U{}*X{}+V{}*Y{}) << ".\n";
 
-	static_assert(std::is_same<decltype(E{}^E{}), E>::value
-			&& std::is_same<decltype(E{}^X{}), E>::value
-			&& std::is_same<decltype(U{}^E{}), E>::value,
+	static_assert(isSame(E{}^E{}, E{}) && isSame(E{}^X{}, E{}) && isSame(U{}^E{}, E{}),
 		"The empty tag must be the multiplicative zero");
-	oss << "Outer product (operator^, operator%):" << para
+
+	auto outers = make_tuple(
+		make_tuple("0 ^ X", E{}, X{}), make_tuple("U ^ 0", U{}, E{}),
+		make_tuple("U ^ X", U{}, X{}), make_tuple("UV ^ X", UV{}, X{}),
+		make_tuple("U ^ XY", U{}, XY{}), make_tuple("UV ^ XYZ", UV{}, XYZ{}),
+		make_tuple("UVW ^ XYZ", UV{}, XYZ{}));
+	auto print_outer = [] (auto && f, auto & os) {
+		os << "  " << get<0>(f) << " = " << pretty(get<1>(f) ^ get<2>(f)) << ";\n"; };
+
+	oss << "Outer product (operator^):" << endl;
+	for_each(outers, print_outer, oss);
+	/*oss << "Outer product (operator^, operator%):" << para
 			<< "0 ^ X = " << pretty(E{}^X{}) << ";" << para
 			<< "U ^ 0 = " << pretty(U{}^E{}) << ";" << para
 			<< "U ^ X = " << pretty(U{}^X{}) << ";" << para
@@ -115,8 +155,8 @@ int main(int argc, const char *argv[]) {
 			<< "(U+V) ^ (X+Y) = " << pretty(UV{} ^ XY{}) << "." << para
 			<< "(U+V+W) ^ (X+Y) = " << pretty(UVW{} ^ XY{}) << "." << para
 			<< "(U+V) ^ (X+Y+Z) = " << pretty(UV{} ^ XYZ{}) << "." << para
-			<< "(U+V+W) ^ (X+Y+Z) = " << pretty(UVW{} ^ XYZ{}) << ".\n"
-		"Transposed outer product:" << para
+			<< "(U+V+W) ^ (X+Y+Z) = " << pretty(UVW{} ^ XYZ{}) << ".\n"*/
+	oss << "Transposed outer product:" << para
 			<< "0 % X = " << pretty(E{} % X{}) << ";" << para
 			<< "U % 0 = " << pretty(U{} % E{}) << ";" << para
 			<< "U % X = " << pretty(U{} % X{}) << ";" << para
@@ -141,12 +181,10 @@ int main(int argc, const char *argv[]) {
 			<< "[U+V]_0 . 1 = " << (access<0>(uv) = 1) << ";" << para
 			<< "[U+V]_1 . 2 = " << (access<1>(uv) = 2) << ".\n";
 
-	static_assert(std::is_same<decltype(reverse(UVW{})), decltype(W{}+V{}+U{})>::value,
+	static_assert(isSame(reverse(UVW{}), W{}+V{}+U{}),
 		"Reverse must distribute over addition");
-	static_assert(std::is_same<decltype(reverse(UV{}^XY{})),
-				decltype(reverse(UV{})^reverse(XY{}))>::value
-			&& std::is_same<decltype(reverse(UV{}*XY{})),
-				decltype(reverse(UV{})*reverse(XY{}))>::value,
+	static_assert(isSame(reverse(UV{}^XY{}), reverse(UV{})^reverse(XY{}))
+			&& isSame(reverse(UV{}*XY{}), reverse(UV{})*reverse(XY{})),
 		"Reverse must distribute over multiplication");
 	oss << "Reversal:" << para
 			<< "rev(U+V) = " << pretty(reverse(UV{})) << ";" << para
@@ -180,62 +218,36 @@ int main(int argc, const char *argv[]) {
 
 	/* There are false positives to this test, but extreme template arguments like this could be
 	 * a preliminary test of recursion limits - TODO create an implementation that fails. */
-	static_assert(std::is_same<decltype(UVW{} << SeqL<9000>{}), UVW>::value &&
-				std::is_same<decltype(UVW{} >> SeqL<9000>{}), UVW>::value,
+	static_assert(isSame(UVW{} << SeqL<9000>{}, UVW{}) &&
+				isSame(UVW{} >> SeqL<9000>{}, UVW{}),
 			"Forward and backward rotations must be cyclic");
-	static_assert(std::is_same<decltype(UVW{} << SeqL<-5>{}),
-		decltype(UVW{} >> SeqL<5>{})>::value, "Negative and inverse rotation must be identical");
-
-	/*
-	auto f_uvw = [] (auto && x, auto && y) {
-		return "(U+V+W)^" + pretty(y) + " = " + pretty(x << y);
-	};
-	auto g_uvw = [=] (auto && x) { return f(get<0>(x), get<1>(x)); };
+	static_assert(isSame(UVW{} << SeqL<-5>{}, UVW{} >> SeqL<5>{}),
+			"Negative and inverse rotation must be identical");
 
 	auto inc_uvw = IncSeq<UVW::size, true, long>{};
 	auto rep_uvw = RepeatSeq<UVW::size, long, -UVW::size/2>{};
-
 	auto diff_uvw = inc_uvw - rep_uvw;
-	auto tag_uvw = seq_to_tag_seq(diff_uvw);
-	auto zip_uvw = zip_tag(UVW{}, tag_uvw);
-	auto tup_uvw = tag_to_tuple(zip_uvw);
-	auto tup_str = transform(tup_uvw, g_uvw);
-	//auto tup_str = transform(inc_uvw, tup_uvw, rot_uvw);
-	*/
 
-	oss << "Rotation:" << para
-			/*<< "rot(U+V+W) = " << pretty(rotate(UVW{})) << ";" << para
-			<< "rot^2(U+V+W) = " << pretty(rotate(rotate(UVW{}))) << ";" << para
-			<< "rot^3(U+V+W) = " << pretty(rotate(rotate(rotate(UVW{})))) << ";" << para*/
-			<< "(U+V+W)^-9 = " << pretty(UVW{} << SeqL<-9>{}) << ';' << para
-			<< "(U+V+W)^-8 = " << pretty(UVW{} << SeqL<-8>{}) << ';' << para
-			<< "(U+V+W)^-7 = " << pretty(UVW{} << SeqL<-7>{}) << ';' << para
-			<< "(U+V+W)^-6 = " << pretty(UVW{} << SeqL<-6>{}) << ';' << para
-			<< "(U+V+W)^-5 = " << pretty(UVW{} << SeqL<-5>{}) << ';' << para
-			<< "(U+V+W)^-4 = " << pretty(UVW{} << SeqL<-4>{}) << ';' << para
-			<< "(U+V+W)^-3 = " << pretty(UVW{} << SeqL<-3>{}) << ';' << para
-			<< "(U+V+W)^-2 = " << pretty(UVW{} << SeqL<-2>{}) << ';' << para
-			<< "(U+V+W)^-1 = " << pretty(UVW{} << SeqL<-1>{}) << ';' << para
-			<< "(U+V+W)^0  = " << pretty(UVW{} << SeqL<0>{}) << ';' << para
-			<< "(U+V+W)^1  = " << pretty(UVW{} << SeqL<1>{}) << ';' << para
-			<< "(U+V+W)^2  = " << pretty(UVW{} << SeqL<2>{}) << ';' << para
-			<< "(U+V+W)^3  = " << pretty(UVW{} << SeqL<3>{}) << ';' << para
-			<< "(U+V+W)^4  = " << pretty(UVW{} << SeqL<4>{}) << ';' << para
-			<< "(U+V+W)^5  = " << pretty(UVW{} << SeqL<5>{}) << ';' << para
-			<< "(U+V+W)^6  = " << pretty(UVW{} << SeqL<6>{}) << '.';
+	oss << "Rotation:" << endl;
+	Detail::for_each(IncSeq<10, true, long>{} - RepeatSeq<10, long, 5>{},
+			PrintShift{}, UVW{}, oss);
 
 	auto res = oss.str();
 
-	using S2 = array<string, 2>;
-	for(auto const& it : vector<S2>{{"->", "-%"}, {"Detail::", ""}, {"Tag", ""},
-			{"std::tuple", "Val"}, {"float", "f"}, {"double", "d"},
-			{"std::integral_constant<unsigned int, ", "Const<"},
-			{"> >", ">>"}, {"<", "{"}, {">", "}"}, {"-%", "->"},
-			{"}, ", "},_"}, {", ", ","}, {"},_","}, "}
-			}) {
+	string repls[][2] = {
+		{" << ", " ## "}, {" >> ", " @@ "}, {"->", "-%"},
+		{"Detail::", ""}, {"Tag", ""},
+		{"std::tuple", "Val"}, {"float", "f"}, {"double", "d"},
+		{"std::integral_constant<unsigned int, ", "Const<"},
+		{"> >", ">>"}, {"<", "{"}, {">", "}"}, {"-%", "->"},
+		{"##", "<<"}, {"@@", ">>"},
+		{"}, ", "},_"}, {", ", ","}, {"},_","}, "}
+	};
+	for(auto const& it : repls) {
 		std::size_t pos;
 		while((pos = res.find(it[0])) != std::string::npos)
 			res.replace(pos, it[0].length(), it[1]);
 	}
 	cout << res << endl;
+
 }
